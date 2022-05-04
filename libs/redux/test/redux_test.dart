89c40ed23714f61870ec0c0169f5733c0ff52925
Michael du Plessis <mdp0101@gmail.com>
2022-05-04 10:03:12 +0200

    :bug: Removed unused import

--- libs/redux/test/redux_test.dart
+++ libs/redux/test/redux_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:redux/redux.dart';
+// import 'package:redux/redux.dart'; // uncommment when tests are implemented
 
 void main() {
   test("Mock test does nothing", () {
