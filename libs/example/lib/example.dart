93d2ee84a0fe782a22616a39a995513c25f5783d
Michael du Plessis <mdp0101@gmail.com>
2022-05-05 18:49:43 +0200

    :sparkles: Added actions folder and file for redux lib

--- libs/example/lib/example.dart
+++ libs/example/lib/example.dart
@@ -1,7 +1,9 @@
 library example;
+import 'package:redux_comp/redux_comp.dart';
 
 /// A Calculator.
 class Calculator {
+  AppState test = AppState();
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
