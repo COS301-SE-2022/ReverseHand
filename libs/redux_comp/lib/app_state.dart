fb9cd538934d14a9d5759f960c13c79de96dc3ea
Michael du Plessis <mdp0101@gmail.com>
2022-05-08 20:11:12 +0200

    :clown_face: Example of how to add data to the appstate

--- libs/redux_comp/lib/app_state.dart
+++ libs/redux_comp/lib/app_state.dart
@@ -1,6 +1,12 @@
 class AppState {
+  // put all app state requiered here
+  final String example; // remove later
+
+  // constructor must only take named parameters
+  AppState({required this.example});
+
   // this methods sets the starting state for the store
   static AppState initial() {
-    return AppState();
+    return AppState(example: "example");
   }
 }
