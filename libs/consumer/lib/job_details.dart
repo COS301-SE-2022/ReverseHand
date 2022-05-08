2dcaf13d54fdcc5e8771b4685560b45c34c1fa0f
arlo216 <charlottevzyl1@gmail.com>
2022-05-08 17:33:19 +0200

    💄 Update of card widget

--- libs/consumer/lib/job_details.dart
+++ libs/consumer/lib/job_details.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import './job_listings.dart';
 
-
-
 class ConsumerJobDetails extends StatefulWidget {
   const ConsumerJobDetails({Key? key}) : super(key: key);
 
@@ -20,8 +18,10 @@ class ConsumerJobDetailsState extends State<ConsumerJobDetails> {
           leading: IconButton(
             icon: const Icon(Icons.arrow_back),
             onPressed: () {
-              Navigator.push(context,
-                MaterialPageRoute(builder: (_) => const ConsumerJobListings()));
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (_) => const ConsumerJobListings()));
             },
           ),
           title: const Text('Roof Painting'),
@@ -38,72 +38,72 @@ class ConsumerDetailsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: 
-        Row(
-          children: [
-            Expanded(
-              child: Column(
-                children: [ 
-                  Card(
-                    color: const Color.fromARGB(255, 86, 159, 92),
-                    shape: RoundedRectangleBorder(
+      body: Row(
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                Card(
+                  color: const Color.fromARGB(82, 121, 111, 92),
+                  shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    elevation: 2,
-                    child: Column(
-                      children: [
-                        const ListTile(
-                          title: Text(
-                            "Description",
-                            style: TextStyle(fontSize: 25.0, color: Colors.white),
-                          ),
+                  ),
+                  elevation: 2,
+                  child: Column(
+                    children: [
+                      const ListTile(
+                        title: Text(
+                          "Description",
+                          style: TextStyle(fontSize: 25.0, color: Colors.white),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
-                          child: Text(
-                            'Greyhound divisively hello coldly wonderfully marginally far upon excluding.',
-                            style: TextStyle(color: Colors.white.withOpacity(0.9)),
-                          ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                        child: Text(
+                          'Greyhound divisively hello coldly wonderfully marginally far upon excluding.',
+                          style:
+                              TextStyle(color: Colors.white.withOpacity(0.9)),
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                  Divider(
-                    height: 15,
-                    thickness: 2,
-                    indent: 10,
-                    endIndent: 10,
-                    color: Colors.black.withOpacity(0.2),
-                  ),
-                  const Text(
-                    "Bids",
-                    style: TextStyle(fontSize: 25.0, color: Colors.black),
-                  ),
-                  // ListView(
-                  //   children: [
-                  //     Card(
-                  //       color: const Color.fromARGB(255, 86, 159, 92),
+                ),
+                Divider(
+                  height: 15,
+                  thickness: 2,
+                  indent: 10,
+                  endIndent: 10,
+                  color: Colors.black.withOpacity(0.2),
+                ),
+                const Text(
+                  "Bids",
+                  style: TextStyle(fontSize: 25.0, color: Colors.black),
+                ),
+                // ListView(
+                //   children: [
+                //     Card(
+                //       color: const Color.fromARGB(255, 86, 159, 92),
 
-                  //       elevation: 2,
-                  //       child: Column(
-                  //         children: const [
-                  //           ListTile(
-                  //             title: Text(
-                  //               'Roof painting',
-                  //               style: TextStyle(fontSize: 25.0, color: Colors.white),
-                  //             ),
-                  //           ),
-                  //         ],
-                  //       ),
+                //       elevation: 2,
+                //       child: Column(
+                //         children: const [
+                //           ListTile(
+                //             title: Text(
+                //               'Roof painting',
+                //               style: TextStyle(fontSize: 25.0, color: Colors.white),
+                //             ),
+                //           ),
+                //         ],
+                //       ),
 
-                  //     ),
-                  //   ],
-                  // ),
-                ],
-              ),
+                //     ),
+                //   ],
+                // ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
     );
   }
 }
