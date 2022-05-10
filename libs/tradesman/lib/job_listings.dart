3826aac7b370aaee15403b7aad9a72a4e50de397
Alexander-Muendesi <u20426918@tuks.co.za>
2022-05-10 14:02:22 +0200

    🐛 Commented out unused imports

--- libs/tradesman/lib/job_listings.dart
+++ libs/tradesman/lib/job_listings.dart
@@ -3,7 +3,7 @@ import 'package:flutter_redux/flutter_redux.dart';
 import './job_details.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_comp/redux_comp.dart';
-import 'package:flutter/rendering.dart';
+//import 'package:flutter/rendering.dart';
 
 class TradesmanJobListings extends StatelessWidget {
   final Store<AppState> store;
@@ -11,11 +11,10 @@ class TradesmanJobListings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     return StoreProvider<AppState>(
-      store: store,
-      child: 
-      MaterialApp(
-        home: Scaffold(
+    return StoreProvider<AppState>(
+        store: store,
+        child: MaterialApp(
+            home: Scaffold(
           appBar: AppBar(
             title: const Text('Job Listings'),
             backgroundColor: const Color.fromRGBO(82, 121, 111, 1),
@@ -24,8 +23,10 @@ class TradesmanJobListings extends StatelessWidget {
             children: [
               InkWell(
                 onTap: () {
-                  Navigator.push(context,
-                    MaterialPageRoute(builder: (_) => TradesmanJobDetails(store: store)));
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => TradesmanJobDetails(store: store)));
                 },
                 child: Card(
                   color: const Color.fromARGB(255, 86, 159, 92),
@@ -41,7 +42,8 @@ class TradesmanJobListings extends StatelessWidget {
                           padding: const EdgeInsets.fromLTRB(0, 10, 15, 0),
                           child: Text(
                             '2 Days ago',
-                            style: TextStyle(color: Colors.white.withOpacity(0.6)),
+                            style:
+                                TextStyle(color: Colors.white.withOpacity(0.6)),
                           ),
                         ),
                       ),
@@ -53,14 +55,16 @@ class TradesmanJobListings extends StatelessWidget {
                         subtitle: Text(
                           '22 May 2022 - 28 May 2022',
                           style: TextStyle(
-                              color: Colors.white.withOpacity(0.7), fontSize: 15.0),
+                              color: Colors.white.withOpacity(0.7),
+                              fontSize: 15.0),
                         ),
                       ),
                       Padding(
                         padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
                         child: Text(
                           'Greyhound divisively hello coldly wonderfully marginally far upon excluding.',
-                          style: TextStyle(color: Colors.white.withOpacity(0.9)),
+                          style:
+                              TextStyle(color: Colors.white.withOpacity(0.9)),
                         ),
                       ),
                     ],
@@ -69,8 +73,6 @@ class TradesmanJobListings extends StatelessWidget {
               ),
             ],
           ),
-        )
-      )
-    );
+        )));
   }
 }
