9342011e8b098e33c6fb7500335095025c59f4c7
Alexander-Muendesi <u20426918@tuks.co.za>
2022-05-12 01:21:50 +0200

    ✅ Added TradesmanJobListings Widget Test

--- libs/tradesman/test/tradesman_test.dart
+++ libs/tradesman/test/tradesman_test.dart
@@ -1,7 +1,33 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:async_redux/async_redux.dart';
+import 'package:flutter/material.dart';
+import 'package:redux_comp/redux_comp.dart';
+import 'package:tradesman/tradesman.dart';
 
 void main() {
-  test("Mock test does nothing", () {
-    expect(true, true);
+  //Create a store to use for the various states
+  final store = Store<AppState>(initialState: AppState.mock());
+
+  //test for TradesmanJobListings
+  testWidgets("Testing TradesmanJobListings class",
+      (WidgetTester tester) async {
+    final store = Store<AppState>(initialState: AppState.mock());
+
+    await tester.pumpWidget(TradesmanJobListings(store: store));
+
+    //find exactly one scaffold widget
+    var scaff = find.byType(Scaffold);
+    expect(scaff, findsOneWidget);
+
+    //find one appbar
+    var appbar = find.descendant(of: scaff, matching: find.byType(AppBar));
+    expect(appbar, findsOneWidget);
+
+    //find exactly one Job Listings Text
+    var text = find.text("Job Listings");
+    expect(text, findsOneWidget);
+
+    var list = find.descendant(of: scaff, matching: find.byType(ListView));
+    expect(list, findsOneWidget);
   });
 }
