c647554e29681c89583811b4e8e1b97bf3007f9f
Alexander-Muendesi <u20426918@tuks.co.za>
2022-05-12 00:03:55 +0200

    🐛 Fixed issue with Widget Test for Login

--- libs/general/test/general_test.dart
+++ libs/general/test/general_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:async_redux/async_redux.dart';
-import 'package:consumer/consumer.dart';
 import 'package:flutter/material.dart';
 import 'package:general/login.dart';
 import 'package:redux_comp/redux_comp.dart';
