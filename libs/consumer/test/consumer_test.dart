81b68ad57de5972807a47fbb2ee161bd615cf778
Alexander-Muendesi <u20426918@tuks.co.za>
2022-05-11 15:44:57 +0200

    ✅ Added widget tests for Consumer Listings

--- libs/consumer/test/consumer_test.dart
+++ libs/consumer/test/consumer_test.dart
@@ -1,10 +1,49 @@
+import 'package:consumer/consumer.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-// import 'package:redux/redux.dart'; // uncommment when tests are implemented
-
-//import 'package:consumer/consumer.dart';
+import 'package:redux/redux.dart';
+import 'package:redux_comp/redux_comp.dart';
 
 void main() {
-  test("Mock test does nothing", () {
-    expect(true, true);
+  final store = Store<AppState>(appReducer, initialState: AppState.initial());
+
+  testWidgets("Testing Title from ConsumerListings",
+      (WidgetTester tester) async {
+    //creating the ConsumerListings widget
+    await tester.pumpWidget(ConsumerListings(store: store));
+
+    //find the Title widget
+    final titleFinder = find.text("My Job Listings");
+
+    expect(titleFinder, findsOneWidget); //expect to find one such title
+
+    final icons = find.byType(Icon);
+    expect(icons, findsWidgets); //expect to find one or more listings
+
+    //attempt to click an advert
+
+    final scaffhold = find.byType(Scaffold);
+
+    // get the appbar
+    final appbar =
+        find.descendant(of: scaffhold, matching: find.byType(AppBar));
+    expect(appbar, findsOneWidget);
+
+    //get the text of the appbar
+    final appText = find.descendant(of: appbar, matching: find.byType(Text));
+    expect(appText, findsOneWidget); //expect one title in appbar
+
+    final des = find.byType(ListTile);
+    expect(des, findsWidgets); //should find more than one title widget
+
+    //find the body
+    final body =
+        find.descendant(of: scaffhold, matching: find.byType(ListView));
+
+    expect(body, findsOneWidget);
+
+    //get the InkWell
+    final ink = find.descendant(of: body, matching: find.byType(InkWell));
+    expect(ink, findsWidgets);
   });
 }
