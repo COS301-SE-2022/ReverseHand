bd72f1cf27f072418e4a4f865118a7be0f78d714
Alexander-Muendesi <u20426918@tuks.co.za>
2022-05-12 22:13:41 +0200

    ✅ Added test for Job Creation Class.

--- libs/consumer/test/consumer_test.dart
+++ libs/consumer/test/consumer_test.dart
@@ -26,4 +26,58 @@ void main() {
     //expect to find exactly one appbar
     expect(_appbar, findsOneWidget);
   });
+
+  testWidgets(("Testing the Job Creation Class"), (WidgetTester tester) async {
+    //create an instance of JobCreation
+    await tester.pumpWidget(JobCreation(store: store));
+
+    //find the IconButton
+    final iconB = find.byType(IconButton);
+    //expect to find one IcconButton
+    expect(iconB, findsOneWidget);
+
+    //find Text element with Creating a job
+    final cJob = find.widgetWithText(Scaffold, "Creating a job");
+    expect(cJob, findsOneWidget);
+
+    //verify there is more than one Textfield on the screen
+    debugPrint("Verifying there are 4 TextFields to enter input");
+    final textf = find.byType(TextField);
+    expect(textf, findsNWidgets(4));
+    debugPrint("Success: Found more exactly 4 TextFields\n");
+
+    //verify there is a button that says "Add new job"
+    debugPrint("Searching for the \"Add New Job\" button");
+    final newJob = find.widgetWithText(Scaffold, "Add New Job");
+    expect(newJob, findsOneWidget);
+    debugPrint("Success: Button to add new job found.\n");
+
+    debugPrint("Locating the Title textfiled");
+    final title = find.widgetWithText(Scaffold, "Title");
+    expect(title, findsOneWidget);
+    debugPrint("Successfully located\n");
+
+    debugPrint("Entering \"Plumbing Job\" in the Title");
+    await tester.enterText(find.widgetWithText(TextField, "Title"), "John");
+    debugPrint("Successfully Entered Title\n");
+
+    debugPrint("Entering Description");
+    await tester.enterText(
+        find.widgetWithText(TextField, "Description"), "Toilet is clogged");
+    debugPrint("Successfully Entered Description\n");
+
+    debugPrint("Entering Location");
+    await tester.enterText(
+        find.widgetWithText(TextField, "Location"), "Pretoria");
+    debugPrint("Successfully Entered Location\n");
+
+    debugPrint("Entering the date");
+    await tester.enterText(
+        find.widgetWithText(TextField, "Date"), "2020.05.12");
+    debugPrint("Successfully Entered Date\n");
+
+    debugPrint("Clicking button to create job");
+    await tester.press(newJob);
+    debugPrint("Job Successfully created\n");
+  });
 }
