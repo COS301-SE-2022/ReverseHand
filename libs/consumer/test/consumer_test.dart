3c52a22c07d98a6023ebce66311f2a7dc0f61e8d
Alexander-Muendesi <u20426918@tuks.co.za>
2022-05-12 21:45:15 +0200

    ✅ Fixing ConsumerListings test

--- libs/consumer/test/consumer_test.dart
+++ libs/consumer/test/consumer_test.dart
@@ -14,59 +14,16 @@ void main() {
 
     //find the Title widget
     final titleFinder = find.text("My Job Listings");
-
     expect(titleFinder, findsOneWidget); //expect to find one such title
 
-    // final icons = find.byType(Icon);
-    // expect(icons, findsWidgets); //expect to find one or more listings
+    //find the scaffhold
+    final scaff = find.byType(Scaffold);
+    //epxect to find exactly one Scaffold
+    expect(scaff, findsOneWidget);
 
-    //attempt to click an advert
-
-    final scaffhold = find.byType(Scaffold);
-
-    // get the appbar
-    final appbar =
-        find.descendant(of: scaffhold, matching: find.byType(AppBar));
-    expect(appbar, findsOneWidget);
-
-    //get the text of the appbar
-    final appText = find.descendant(of: appbar, matching: find.byType(Text));
-    expect(appText, findsOneWidget); //expect one title in appbar
-
-    // final des = find.byType(ListTile);
-    // expect(des, findsWidgets); //should find more than one title widget
-
-    //find the body
-    // final body =
-    //     find.descendant(of: scaffhold, matching: find.byType(ListView));
-
-    // expect(body, findsOneWidget);
-
-    // //get the InkWell
-    // final ink = find.descendant(of: body, matching: find.byType(InkWell));
-    // expect(ink, findsWidgets);
+    //AppBar must be a descendant of the Scaffhold
+    final _appbar = find.descendant(of: scaff, matching: find.byType(AppBar));
+    //expect to find exactly one appbar
+    expect(_appbar, findsOneWidget);
   });
-
-/*
-  testWidgets("Widget test for Consumer Details", (WidgetTester tester) async {
-    //setting up the ConsumerDetails widget
-    await tester.pumpWidget(ConsumerDetails(store: store));
-
-    final scaffhold = find.byType(Scaffold);
-    expect(scaffhold, findsOneWidget);
-
-    //find the back button
-    final iconButton =
-        find.descendant(of: scaffhold, matching: find.byType(IconButton));
-    expect(iconButton, findsOneWidget);
-
-    //find the row element
-    final row = find.descendant(of: scaffhold, matching: find.byType(Row));
-    expect(row, findsOneWidget);
-
-    //check for description
-    final desc = find.descendant(of: row, matching: find.text("Description"));
-    expect(desc, findsOneWidget);
-  });
-*/
 }
