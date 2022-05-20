31892524f171ab90d87cf1e3595bd55a0db6e94a
lastrucci01 <u20430168@tuks.co.za>
2022-05-20 15:09:21 +0200

    :clown_face: Added Mock test

--- libs/amplify/test/amplify_test.dart
+++ libs/amplify/test/amplify_test.dart
@@ -3,10 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:amplify/amplify.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('Mock Test: Amplify', () {
+    expect(true, true);
   });
 }
