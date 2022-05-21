965b134d94ae547046cf19c243472a4f13a2cf43
Michael du Plessis <mdp0101@gmail.com>
2022-05-21 14:13:57 +0200

    :bug: Added immutable tag to subclasses of UserModel

--- libs/redux_comp/lib/models/user_models/consumer_model.dart
+++ libs/redux_comp/lib/models/user_models/consumer_model.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:redux_comp/models/user_model.dart';
 
+@immutable
 class ConsumerModel extends UserModel {
   ConsumerModel(String name) : super(name);
 }
--- libs/redux_comp/lib/models/user_models/tradesman_model.dart
+++ libs/redux_comp/lib/models/user_models/tradesman_model.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:redux_comp/models/user_model.dart';
 
+@immutable
 class TradesmanModel extends UserModel {
   TradesmanModel(String name) : super(name);
 }
