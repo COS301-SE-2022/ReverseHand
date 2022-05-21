84756914dc7baa344ade58918d840975674d60e0
Michael du Plessis <mdp0101@gmail.com>
2022-05-21 14:29:30 +0200

    :pencil2: getEmail now returns email and changed immutable classes constructors to const

--- libs/redux_comp/lib/models/user_model.dart
+++ libs/redux_comp/lib/models/user_model.dart
@@ -5,13 +5,13 @@ abstract class UserModel {
   final String _name;
   final String _email;
 
-  UserModel(this._name, this._email);
+  const UserModel(this._name, this._email);
 
   String getName() {
     return _name;
   }
 
   String getEmail() {
-    return _name;
+    return _email;
   }
 }
--- libs/redux_comp/lib/models/user_models/consumer_model.dart
+++ libs/redux_comp/lib/models/user_models/consumer_model.dart
@@ -3,5 +3,5 @@ import 'package:redux_comp/models/user_model.dart';
 
 @immutable
 class ConsumerModel extends UserModel {
-  ConsumerModel(String name, String email) : super(name, email);
+  const ConsumerModel(String name, String email) : super(name, email);
 }
