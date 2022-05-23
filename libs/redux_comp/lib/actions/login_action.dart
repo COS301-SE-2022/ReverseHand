b4064787d7a2a32ab1ee152fa954d50e7cb1dec4
lastrucci01 <u20430168@tuks.co.za>
2022-05-23 10:55:40 +0200

    :bug: Refactored existing code for new features (Cognito, seperate models)

--- libs/redux_comp/lib/actions/login_action.dart
+++ libs/redux_comp/lib/actions/login_action.dart
@@ -1,10 +1,13 @@
 import 'dart:async';
-import 'package:amplify/models/Advert.dart';
-import 'package:amplify/models/Consumer.dart';
-import 'package:amplify_api/amplify_api.dart';
+// import 'package:amplify/models/Advert.dart';
+// import 'package:amplify/models/Consumer.dart';
+// // import 'package:amplify_api/amplify_api.dart';
+// import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:async_redux/async_redux.dart';
 import 'package:redux_comp/app_state.dart';
+
+import '../models/user_models/consumer_model.dart';
 
 class LoginAction extends ReduxAction<AppState> {
   final String email;
@@ -12,34 +15,19 @@ class LoginAction extends ReduxAction<AppState> {
 
   LoginAction(this.email, this.password);
 
-  @override
-  Future<AppState?> reduce() async {
-    try {
-      final requestID = ModelQueries.list(Consumer.classType,
-          where: Consumer.EMAIL.contains(email));
-
-      final responseID = await Amplify.API.query(request: requestID).response;
-      
-        String id = responseID.data!.items[0]!.id;
-        String? username = responseID.data!.items[0]!.email;
-      
-      final requestAdverts = ModelQueries.list(Advert.classType,
-          where: Advert.CONSUMERID.contains(id));
-
-      final responseAdverts = await Amplify.API.query(request: requestAdverts).response;
-    
-      List<Advert>? consumerAdverts = [];
-
-      for (Advert? advert in responseAdverts.data!.items) {
-        consumerAdverts.add(advert!);
-      }
-      
-      return state.replace(
-        id: id, 
-        username: username,
-        adverts: consumerAdverts
-        );
+    @override
+    Future<AppState?> reduce() async {
+      try {
+      await Amplify.Auth.signIn(
+      username: email,
+      password: password,
+    );
+    String id = (await Amplify.Auth.getCurrentUser()).userId;
+    return state.replace(
+      user: ConsumerModel(id, email, email)
+    );
       // exception will be handled later
+    //} on AuthException catch (e) {
     } catch (e) {
       return state;
     }
--- libs/redux_comp/lib/actions/view_bids_action.dart
+++
@@ -1,8 +0,0 @@
-import '../app_state.dart';
-import 'package:amplify_flutter/amplify_flutter.dart';
-import 'package:async_redux/async_redux.dart';
-
-class ViewBidsAction extends ReduxAction<AppState> {
-  @override
-  Future<AppState?> reduce() async {}
-}
