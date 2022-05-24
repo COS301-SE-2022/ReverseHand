725e126fba0b24ab01d2befce09a93b5847bb432
lastrucci01 <u20430168@tuks.co.za>
2022-05-24 21:57:59 +0200

    :hankey: Commented packages back

--- libs/redux_comp/lib/actions/register_user_action.dart
+++ libs/redux_comp/lib/actions/register_user_action.dart
@@ -1,7 +1,6 @@
-import 'package:amazon_cognito_identity_dart_2/cognito.dart';
 // import 'package:amplify/amplify.dart';
-// import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
-// import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:async_redux/async_redux.dart';
 // import 'package:flutter/foundation.dart';
 // import 'package:redux_comp/models/user_models/consumer_model.dart';
@@ -15,27 +14,19 @@ class RegisterUserAction extends ReduxAction<AppState> {
   @override
   Future<AppState?> reduce() async {
     try {
-      // Map<CognitoUserAttributeKey, String> userAttributes = {
-      //   CognitoUserAttributeKey.email: 'email@domain.com',
-      //   // additional attributes as needed
-      // };
-      final userPool = CognitoUserPool(
-        'ap-southeast-1_xxxxxxxxx',
-        'k165j5iid3jlctq8uv2naigue',
+      Map<CognitoUserAttributeKey, String> userAttributes = {
+        CognitoUserAttributeKey.email: 'email@domain.com',
+        // additional attributes as needed
+      };
+     await Amplify.Auth.signUp(
+        username: username,
+        password: password,
+        options: CognitoSignUpOptions(
+          userAttributes: userAttributes
+        )
       );
-      final userAttributes = [
-        const AttributeArg(name: 'first_name', value: 'Jimmy'),
-        const AttributeArg(name: 'last_name', value: 'Wong'),
-      ];
-
-      /*CognitoUserPoolData res = */await userPool.signUp(
-        'email@inspire.my',
-        'Password001',
-        userAttributes: userAttributes,
-      );
-
     return state;
-    } catch (e) {
+    } /*on AuthException */ catch (e) {
       return state;
     }
     /*on ApiException catch (e) {
