b499ccd7c0286271b78762a10fd441fa45e8e60f
Michael du Plessis <mdp0101@gmail.com>
2022-05-26 14:24:36 +0200

    :recycle: Changed files to match flutter 3.0 requirments

--- libs/authentication/lib/widgets/button.dart
+++ libs/authentication/lib/widgets/button.dart
@@ -1,32 +1,31 @@
 import 'package:flutter/material.dart';
 
 class LongButtonWidget extends StatelessWidget {
-  final String text; 
+  final String text;
   const LongButtonWidget({Key? key, required this.text}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: 290,
       height: 90,
       child: Column(
-      children: <Widget>[
-        ElevatedButton(
-          child: const Text("LOGIN"), //change to use parameter here
-          style: ElevatedButton.styleFrom(
-            primary: Colors.orange,
-            onPrimary: Colors.white,
-            shadowColor: Colors.black,
-            elevation: 9,
-            textStyle: const TextStyle(fontSize: 20),
-            minimumSize: const Size(400, 50),
-            shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30.0))),
+        children: <Widget>[
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.orange,
+              onPrimary: Colors.white,
+              shadowColor: Colors.black,
+              elevation: 9,
+              textStyle: const TextStyle(fontSize: 20),
+              minimumSize: const Size(400, 50),
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(30.0))),
+            ),
+            onPressed: () {},
+            child: const Text("LOGIN"),
           ),
-          onPressed: () {
-            
-          },
-        ),
-      ],
+        ],
       ),
     );
   }
