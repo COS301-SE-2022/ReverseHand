8daef134f4280a6638cdbd1044e466c3944107e0
Alexander-Muendesi <u20426918@tuks.co.za>
2022-05-27 12:38:46 +0200

    ✅ Added LoginPage widget test

--- libs/authentication/test/authentication_test.dart
+++ libs/authentication/test/authentication_test.dart
@@ -1,67 +1,32 @@
-// import 'package:authentication/pages/login.dart';
+import 'package:async_redux/async_redux.dart';
+import 'package:authentication/pages/login.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
-// import 'package:async_redux/async_redux.dart';
-// import 'package:flutter/material.dart';
-// import 'package:redux_comp/redux_comp.dart';
+import 'package:redux_comp/app_state.dart';
 
 void main() {
-  test("mock", () {
-    expect(true, true);
-  });
-
-  /*
-  //Create a store to use for the various states
   final store = Store<AppState>(initialState: AppState.mock());
-
-  //testing the login class
-  testWidgets(
-      "Testing the login class to see if it holds exactly Login page widget",
-      (WidgetTester tester) async {
+  testWidgets("Testing LoginPage class", (WidgetTester tester) async {
     await tester.pumpWidget(LoginPage(store: store));
 
-    final page = find.byType(LoginPage);
-    expect(page, findsOneWidget);
+    final email = find.text("email");
+    expect(email, findsOneWidget);
+
+    final password = find.text("password");
+    expect(password, findsOneWidget);
+
+    final login = find.text("Login");
+    expect(login, findsOneWidget);
+    expect(find.widgetWithText(Text, "login"), findsNothing);
+    expect(find.text("or"), findsOneWidget);
+
+    final signUp = find.text("Sign Up");
+    expect(signUp, findsOneWidget);
+    expect(find.text("SignUp"), findsNothing);
+
+    final message = find.text("Don't have an account? ");
+    expect(message, findsOneWidget);
+    expect(find.text("or login with:"), findsOneWidget);
+    expect(find.text("Don't have an account?"), findsNothing);
   });
-
-  testWidgets("Testing the widgets in _LoginPageState",
-      (WidgetTester tester) async {
-    var x = LoginPage(store: store);
-    // var y = x.createState();
-    await tester.pumpWidget(x);
-
-    //Expect to find one scaffhold widget
-    var scaffhold = find.byType(Scaffold);
-    expect(scaffhold, findsOneWidget);
-
-    //expect to find login page string in the page
-    var loginPage = find.text("Login Page");
-    expect(loginPage, findsOneWidget);
-
-    //expect to find one SingleChildScrollView
-    var scroll = find.descendant(
-        of: scaffhold, matching: find.byType(SingleChildScrollView));
-    expect(scroll, findsOneWidget);
-
-    //expect to find 2 TextFields
-    var textfield =
-        find.descendant(of: scroll, matching: find.byType(TextField));
-
-    expect(textfield, findsNWidgets(2));
-
-    //Find a textField with email in it
-    expect(find.widgetWithText(TextField, 'Email'), findsOneWidget);
-
-    //find one widget with text Password
-    expect(find.widgetWithText(TextField, 'Password'), findsOneWidget);
-
-    //push the login button
-    await tester.tap(find.widgetWithText(TextButton, "Login"));
-
-    //wait for the push of login button
-    await tester.pumpAndSettle();
-
-    //should see something with My Job Listing on the page if success
-    expect(find.text("My Job Listings"), findsOneWidget);
-  });
-  */
 }
