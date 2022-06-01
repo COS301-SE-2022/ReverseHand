070497cfd8c31dc26882e2683625353e90456890
caaaramia <u20478144@tuks.co.za>
2022-06-01 10:10:14 +0200

    ✨ Created shared popup widget class

---
+++ libs/general/lib/widgets/pop_up.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PopUpWidget extends StatelessWidget {
+  const PopUpWidget({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
