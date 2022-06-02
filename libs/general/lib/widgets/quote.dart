6294dca839d77a5a200b5e134a1044bf97460cfe
arlo216 <charlottevzyl1@gmail.com>
2022-06-02 21:35:37 +0200

    💄 Changed quote widget to be a button and added better spacing

--- libs/general/lib/widgets/quote.dart
+++ libs/general/lib/widgets/quote.dart
@@ -5,45 +5,38 @@ class QuoteWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return Padding(
-    //     padding: const EdgeInsets.only(top: 20.0, left: 60.0, bottom: 10),
-    //     child: Container(
-    //       padding: const EdgeInsets.all(8),
-    //       alignment: Alignment.bottomCenter,
-    //       decoration: const BoxDecoration(
-    //           color: Colors.white,
-    //           borderRadius: BorderRadius.all(Radius.circular(20.0))),
-    //       child: Padding(
-    //         padding: const EdgeInsets.all(3.0),
-    //         child: Row(children: const <Widget>[
-    //           Icon(
-    //             Icons.file_copy_rounded,
-    //             color: Colors.black,
-    //             size: 25.0,
-    //             semanticLabel: 'Text to announce in accessibility modes',
-    //           ),
-    //           Padding(padding: EdgeInsets.all(2)),
-    //           Text(
-    //             "Download Quote",
-    //             style: TextStyle(fontSize: 25, color: Colors.black),
-    //             textAlign: TextAlign.center,
-    //           ),
-    //         ]),
-    //       ),
-    //     ));
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        primary: Colors.white,
-        onPrimary: Colors.white,
-        shadowColor: Colors.black,
-        elevation: 9,
-        textStyle: const TextStyle(fontSize: 20),
-        minimumSize: const Size(200, 50),
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(30.0))),
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0, left: 50.0, bottom: 10),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.white,
+          shadowColor: Colors.black,
+          elevation: 9,
+          textStyle: const TextStyle(fontSize: 20),
+          minimumSize: const Size(200, 50),
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30.0))),
+        ),
+        onPressed: () {},
+        child: Padding(
+          padding: const EdgeInsets.all(3.0),
+          child: Row(
+            children: const [
+              Icon(
+                Icons.file_copy_rounded,
+                color: Colors.black,
+                size: 25.0,
+              ),
+              Padding(padding: EdgeInsets.all(2)),
+              Text(
+                "Download Quote",
+                style: TextStyle(fontSize: 25, color: Colors.black),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
       ),
-      onPressed: () {},
-      child: Text("Download Quote"),
     );
   }
 }
