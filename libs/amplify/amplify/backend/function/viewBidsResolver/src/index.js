022747708da2f0997e3e6876fae397fe34156339
Michael du Plessis <mdp0101@gmail.com>
2022-06-02 21:31:27 +0200

    :sparkles: Coded resolver for viewing bids

--- libs/amplify/amplify/backend/function/viewBidsResolver/src/index.js
+++ libs/amplify/amplify/backend/function/viewBidsResolver/src/index.js
@@ -1,5 +1,7 @@
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
 
-
+const ReverseHandTable = process.env.REVERSEHAND;
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
@@ -7,7 +9,23 @@
 exports.handler = async (event, context) => {
     console.log(event);
     console.log(context);
-    return {
-        id: "001"
+
+    let params = {
+        TableName: ReverseHandTable,
+        IndexName: "part_id-user_id-index",
+        KeyConditionExpression: "user_id = c#001",
     };
+
+    try {
+        const data = await docClient.query(params).promise();
+
+        return {
+            id: err
+        };
+    } catch {
+        return {
+            id: err
+        };
+    }
+
 };
