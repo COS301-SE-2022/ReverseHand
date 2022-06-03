4768e822b2fb58d3a371109b81c8bb3c7c2df2cf
Michael du Plessis <mdp0101@gmail.com>
2022-06-03 10:25:57 +0200

    :sparkles: Created graphql schema for system

--- libs/amplify/amplify/backend/function/viewBidsResolver/src/index.js
+++ libs/amplify/amplify/backend/function/viewBidsResolver/src/index.js
@@ -7,16 +7,21 @@ const ReverseHandTable = process.env.REVERSEHAND;
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event, context) => {
-    console.log(event);
-    console.log(context);
+    // console.log(event);
+    // console.log(context);
 
     let params = {
         TableName: ReverseHandTable,
-        KeyConditionExpression: "user_id = c#001",
+        KeyConditionExpression: "user_id = :u and sort_key = :s",
+        ExpressionAttributeValues: {
+            ":u": event.arguments.user_id,
+            ":s": event.arguments.ad_id
+        }
     };
 
     try {
-        const data = await docClient.scan(params).promise();
+        const data = await docClient.query(params).promise();
+        console.log(data);
 
         return {
             id: "Hi"
