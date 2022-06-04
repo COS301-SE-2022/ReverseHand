f3b4f6aa24f3dd0d17e858fef709cda8a169015b
caaaramia <u20478144@tuks.co.za>
2022-06-04 19:08:06 +0200

    💡 Updated comments in login

--- libs/authentication/lib/methods/populate_login.dart
+++ libs/authentication/lib/methods/populate_login.dart
@@ -1,13 +1,13 @@
 // import 'package:amplify/models/Advert.dart';
 import 'package:async_redux/async_redux.dart';
 import 'package:authentication/authentication.dart';
-// import 'package:consumer/pages/job_listings.dart';
+import 'package:consumer/pages/job_listings.dart';
 import 'package:flutter/material.dart';
 import 'package:general/theme.dart';
 import 'package:general/widgets/divider.dart';
 import 'package:redux_comp/actions/login_action.dart';
 import 'package:redux_comp/app_state.dart';
-import 'package:tradesman/pages/job_listings.dart';
+// import 'package:tradesman/pages/job_listings.dart';
 import 'dart:ui';
 import '../widgets/button.dart';
 import '../widgets/link.dart';
@@ -116,8 +116,8 @@ class Login extends StatelessWidget {
                         Navigator.push(
                           context,
                           MaterialPageRoute( 
-                              //builder: (_) => ConsumerListings(store: store)),
-                              builder: (_) => TradesmanJobListings(store: store)),
+                              builder: (_) => ConsumerListings(store: store)), //comment to rather view tradesman
+                              //builder: (_) => TradesmanJobListings(store: store)), //uncomment to view tradesman
                         )
                       },
                     );
