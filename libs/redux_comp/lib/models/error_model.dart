45cf329488236bc4ce3b4a443c942a31619fb8a7
Michael du Plessis <mdp0101@gmail.com>
2022-06-05 17:08:03 +0200

    :sparkles: Not found error model created

--- libs/redux_comp/lib/models/error_model.dart
+++ libs/redux_comp/lib/models/error_model.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 
 // export other errrors
 export 'errors/none_error_model.dart';
+export 'errors/not_found_error_model.dart';
 
 @immutable
 abstract class ErrorModel {
-  final String? msg;
+  final String? msg; // msg if one needs to be printed
 
   const ErrorModel({this.msg});
 
+  // even though msg is public call using method to ensure null safety
   String message() {
     return msg ?? "";
   }
