b6bcdcc9970b2f961b766dbe0b792997df4ae260
Michael du Plessis <mdp0101@gmail.com>
2022-06-05 16:47:55 +0200

    :sparkles: Created shortlist_bid action

---
+++ libs/redux_comp/lib/actions/shortlist_bid_action.dart
@@ -0,0 +1,13 @@
+import '../app_state.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:async_redux/async_redux.dart';
+
+class ShortlistBidAction extends ReduxAction<AppState> {
+  final String adId;
+  final String bidId;
+
+  ShortlistBidAction(this.adId, this.bidId);
+
+  @override
+  Future<AppState?> reduce() async {}
+}
