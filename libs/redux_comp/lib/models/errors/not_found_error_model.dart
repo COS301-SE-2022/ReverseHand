06c57b89bc5d1e3c0b1d0c77ad21572dfa0da412
Michael du Plessis <mdp0101@gmail.com>
2022-06-05 23:06:26 +0200

    :recycle: Errors are now represented by enums

--- libs/redux_comp/lib/models/errors/not_found_error_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:redux_comp/models/error_model.dart';
-
-// used when some item is not found but cannot specify what
-// if it is know what cannot be found use the correct error class or create one
-@immutable
-class NotFoundErrorModel extends ErrorModel {
-  const NotFoundErrorModel(String msg) : super(msg: msg);
-}
