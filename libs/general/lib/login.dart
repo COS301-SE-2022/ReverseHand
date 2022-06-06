0983ec449bffe48442907060c87092671bb70ec1
arlo216 <charlottevzyl1@gmail.com>
2022-06-06 11:03:38 +0200

    🚧  attempt to fix use of a private type in public API

--- libs/general/lib/login.dart
+++ libs/general/lib/login.dart
@@ -8,10 +8,10 @@ class LoginPage extends StatefulWidget {
   const LoginPage({Key? key, required this.store}) : super(key: key);
 
   @override
-  _LoginPageState createState() => _LoginPageState();
+  LoginPageState createState() => LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class LoginPageState extends State<LoginPage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
