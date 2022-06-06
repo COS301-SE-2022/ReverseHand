a4362b0bd4040ded01a03d9c35f17f2616b61818
Michael du Plessis <mdp0101@gmail.com>
2022-06-07 00:16:29 +0200

    :hankey: Wrote first attempt at acceptBidResolver

--- libs/amplify/amplify/backend/function/acceptBidResolver/src/index.js
+++ libs/amplify/amplify/backend/function/acceptBidResolver/src/index.js
@@ -1,17 +1,43 @@
-
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+const ReverseHandTable = process.env.REVERSEHAND;
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+
+// add a bid id to an adverts accepted bid
+// requires customer id and advert id and shorlisted bid id
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+    try {
+        let params = {
+            TableName: ReverseHandTable,
+            KeyConditionExpression: "user_id = :u and sort_key = :a",
+            ExpressionAttributeValues: {
+                ":u": event.arguments.user_id,
+                ":a": event.arguments.ad_id
+            }
+        };
+
+        const data = await docClient.query(params).promise();
+        let ad = data["Items"][0]; // should only return one item
+        
+        let item = {
+            TableName: ReverseHandTable,
+            Item: {
+                user_id: event.arguments.ad_id,
+                sort_key: event.arguments.ad_id, // prefixing but keeping same suffix
+                advert_details: {
+                    accepted_bid: sbid_id,
+                }
+            }
+        };
+
+        await docClient.put(item).promise();
+    
+        return item.Item.advert_details;
+    } catch(e) {
+        console.log(e)
+        return e;
+    }
 };
