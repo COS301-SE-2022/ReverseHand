545e8a8831db8797ac0aed47bf75e8e167951e61
Michael du Plessis <mdp0101@gmail.com>
2022-06-06 21:55:17 +0200

    :sparkles: Coded shortListBid resolver

--- libs/amplify/amplify/backend/function/shortListBidResolver/src/index.js
+++ libs/amplify/amplify/backend/function/shortListBidResolver/src/index.js
@@ -1,17 +1,46 @@
-
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+const ReverseHandTable = process.env.REVERSEHAND;
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+
+// recieves an advert as well as a bid id and adds that bid to the shortlisted bids
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+    try {
+        let params = {
+            TableName: ReverseHandTable,
+            KeyConditionExpression: "user_id = :u and sort_key = :s",
+            ExpressionAttributeValues: {
+                ":u": event.arguments.ad_id,
+                ":s": event.arguments.bid_id
+            }
+        };
+
+        const data = await docClient.query(params).promise();
+        let bid = data["Items"][0];
+
+        let item = {
+            TableName: ReverseHandTable,
+            Item: {
+                user_id: event.arguments.ad_id,
+                sort_key: 's' + event.arguments.bid_id, // prefixing but keeping same suffix
+                bid_details: {
+                    price_lower: ibidtem['bid_details']['price_lower'],
+                    price_upper: bid['bid_details']['price_upper'],
+                    quote: item['bid_details']['quote'],
+                    date_created: bid['bid_details']['date_created'],
+                    date_closed: bid['bid_details']['date_closed']
+                }
+            }
+        };
+
+        await docClient.putItem(item);
+    
+        return item.Item;
+    } catch(e) {
+        console.log(e)
+        return e;
+    }
 };
--- libs/amplify/amplify/backend/function/viewBidsResolver/src/index.js
+++ libs/amplify/amplify/backend/function/viewBidsResolver/src/index.js
@@ -1,6 +1,5 @@
 const AWS = require("aws-sdk");
 const docClient = new AWS.DynamoDB.DocumentClient();
-
 const ReverseHandTable = process.env.REVERSEHAND;
 
 /**
@@ -26,13 +25,13 @@ exports.handler = async (event, context) => {
         for (let item of items) {
             bids.push({
                 id: item['sort_key'],
-                advert_id: item['user_id'], // since this is the advert we searched for
+                advert_id: item['bid_details']['user_id'], // since this is the advert we searched for
                 user_id: item['user'],
-                price_lower: item['price_lower'],
-                price_upper: item['price_upper'],
-                quote: item['quote'],
-                date_created: item['date_created'],
-                date_closed: item['date_closed']
+                price_lower: item['bid_details']['price_lower'],
+                price_upper: item['bid_details']['price_upper'],
+                quote: item['bid_details']['quote'],
+                date_created: item['bid_details']['date_created'],
+                date_closed: item['bid_details']['date_closed']
             });
         }
     
