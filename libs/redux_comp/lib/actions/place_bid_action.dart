37b7b028a9327f8d6dc2c6240748c9dbe0115e98
Michael du Plessis <mdp0101@gmail.com>
2022-06-07 16:50:53 +0200

    :recycle: Finished palceBid reduer

--- libs/redux_comp/lib/actions/place_bid_action.dart
+++ libs/redux_comp/lib/actions/place_bid_action.dart
@@ -1,8 +1,46 @@
+import 'package:amplify_api/amplify_api.dart';
+import 'package:uuid/uuid.dart';
 import '../app_state.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:async_redux/async_redux.dart';
 
 class PlaceBidAction extends ReduxAction<AppState> {
-	@override
-	Future<AppState?> reduce() async {}
+  final String adId;
+  final int priceLower;
+  final int priceUpper;
+  final String? quote;
+
+  PlaceBidAction(this.adId, this.priceLower, this.priceUpper, {this.quote});
+
+  @override
+  Future<AppState?> reduce() async {
+    String bidId = const Uuid().v1();
+
+    // type is not used currently but will be implemented in the future
+    String graphQLDocument = '''mutation {
+      placeBid(ad_id: "$adId", bid_id: "$bidId", price_lower: "$priceLower", price_upper: "$priceUpper", quote: "$quote") {
+        id
+        user_id
+        price_lower
+        price_upper
+        quote
+        date_created
+        date_closed
+      }
+    }''';
+
+    final request = GraphQLRequest(
+      document: graphQLDocument,
+    );
+
+    try {
+      await Amplify.API
+          .mutate(request: request)
+          .response; // currently don't need to response return but may be later
+
+      return state; // no change to bid currently
+    } catch (e) {
+      return state;
+    }
+  }
 }
