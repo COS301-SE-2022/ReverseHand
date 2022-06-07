c24ecb52d04e9111a5a648e1e22ac3480411059b
Michael du Plessis <mdp0101@gmail.com>
2022-06-07 14:59:17 +0200

    :hankey: Wrote initial code for placeBidResolver

--- libs/amplify/amplify/backend/function/placeBidResolver/src/index.js
+++ libs/amplify/amplify/backend/function/placeBidResolver/src/index.js
@@ -9,6 +9,13 @@ const ReverseHandTable = process.env.REVERSEHAND;
 // places a bid on an advert, requires the bid to place as well as the id the advert to place the bid
 // created_date will be made in resolver whereas UUID ofbid will be passed in
 exports.handler = async (event) => {
+    // getting current date
+    const date = new Date();
+    const dd = String(date.getDate()).padStart(2, '0');
+    const mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const yyyy = date.getFullYear();
+    const currentDate = mm + '-' + dd + '-' + yyyy;
+
     let item = {
         TableName: ReverseHandTable,
         Item: {
@@ -19,10 +26,12 @@ exports.handler = async (event) => {
                 price_lower: event.arguments.price_lower,
                 price_upper:event.arguments.price_upper,
                 quote: event.arguments.quote, //optional parameter
-                date_created: ,
+                date_created: currentDate,
             }
         }
     };
 
     await docClient.put(item).promise();
+
+    return item.Item.bid_details;
 };
