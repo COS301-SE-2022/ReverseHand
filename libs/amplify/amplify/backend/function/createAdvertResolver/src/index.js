9de139ba023286df02e487d06852bce073cd5dbd
Michael du Plessis <mdp0101@gmail.com>
2022-06-07 14:49:11 +0200

    :twisted_rightwards_arrows: Pulled from amplify

---
+++ libs/amplify/amplify/backend/function/createAdvertResolver/src/index.js
@@ -0,0 +1,48 @@
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+const ReverseHandTable = process.env.REVERSEHAND;
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+
+// creates a new advert, requires
+/*
+    id: uniquely generated, passed in to avoid working with layers
+    customerId
+    title
+*/
+// Optional
+/*
+    description
+    type
+    location // will become required later on
+*/
+exports.handler = async (event) => {
+    // getting current date
+    const date = new Date();
+    const dd = String(date.getDate()).padStart(2, '0');
+    const mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const yyyy = date.getFullYear();
+    const currentDate = mm + '-' + dd + '-' + yyyy;
+
+    let item = {
+        TableName: ReverseHandTable,
+        Item: {
+            user_id: event.arguments.customer_id,
+            sort_key: event.arguments.ad_id, // prefixing but keeping same suffix
+            advert_details: {
+                id: event.arguments.ad_id,
+                title: event.arguments.title,
+                description: event.arguments.description,
+                location: event.arguments.location,
+                type: event.arguments.type,
+                date_created: currentDate // automatically generating the date
+            }
+        }
+    };
+
+    await docClient.put(item).promise();
+
+    return item.Item.advert_details;
+};
--- libs/amplify/amplify/backend/function/shortListBidResolver/src/index.js
+++ libs/amplify/amplify/backend/function/shortListBidResolver/src/index.js
@@ -1,17 +1,49 @@
-
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+const ReverseHandTable = process.env.REVERSEHAND;
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+
+// recieves an advert as well as a bid id and adds that bid to the shortlisted bids
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+    try {
+        let params = {
+            TableName: ReverseHandTable,
+            KeyConditionExpression: "user_id = :u and sort_key = :s",
+            ExpressionAttributeValues: {
+                ":u": event.arguments.ad_id,
+                ":s": event.arguments.bid_id
+            }
+        };
+
+        const data = await docClient.query(params).promise();
+        let bid = data["Items"][0];
+        
+        let shortBidId =  's' + event.arguments.bid_id;
+
+        let item = {
+            TableName: ReverseHandTable,
+            Item: {
+                user_id: event.arguments.ad_id,
+                sort_key: shortBidId, // prefixing but keeping same suffix
+                bid_details: {
+                    id: event.arguments.bid_id,
+                    price_lower: bid['bid_details']['price_lower'],
+                    price_upper: bid['bid_details']['price_upper'],
+                    quote: bid['bid_details']['quote'],
+                    date_created: bid['bid_details']['date_created'],
+                    date_closed: bid['bid_details']['date_closed']
+                }
+            }
+        };
+
+        await docClient.put(item).promise();
+    
+        return item.Item.bid_details;
+    } catch(e) {
+        console.log(e)
+        return e;
+    }
 };
