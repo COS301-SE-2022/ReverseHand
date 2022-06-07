04e8de55ebc13d0043a0a568a8cbfa18fc6cf142
Alexander-Muendesi <u20426918@tuks.co.za>
2022-06-07 14:28:08 +0200

    ✅ SignUp page tested in depth

--- libs/authentication/test/authentication_test.dart
+++ libs/authentication/test/authentication_test.dart
@@ -9,7 +9,7 @@ import 'package:redux_comp/app_state.dart';
 void main() {
   final store = Store<AppState>(initialState: AppState.mock());
 
-  group("Login Widget Test", () {
+  group("Authentication: ", () {
     testWidgets("Testing LoginPage page", (WidgetTester tester) async {
       await tester.pumpWidget(LoginPage(store: store));
 
@@ -47,6 +47,91 @@ void main() {
       await tester.pumpAndSettle();
       expect(find.text("name"),
           findsOneWidget); //this indicates successfully landed on the signup page
+    });
+
+    testWidgets("Testing the Signup page", (WidgetTester tester) async {
+      await tester.pumpWidget(LoginPage(store: store));
+
+      //get the sign up widget at the bottom of the page
+      final signUp = find.widgetWithText(GestureDetector, "Sign Up");
+      expect(signUp, findsOneWidget);
+
+      //press the sign up text and proceed to the signup page
+      await tester.tap(signUp);
+      await tester.pumpAndSettle();
+
+      //check whether we are on signup page by looking for "Sign Up"
+      //text right on top of the page
+      expect(find.text("Sign Up"), findsNWidgets(2));
+
+      //store important widgets to be used
+      var name = find.widgetWithText(TextFieldWidget, "name");
+      expect(name, findsOneWidget);
+      var email = find.widgetWithText(TextFieldWidget, "email");
+      expect(email, findsOneWidget);
+      var cellphone = find.widgetWithText(TextFieldWidget, "cellphone");
+      expect(cellphone, findsOneWidget);
+      var location = find.widgetWithText(TextFieldWidget, "location");
+      expect(location, findsOneWidget);
+      var password = find.widgetWithText(TextFieldWidget, "password");
+      expect(password, findsOneWidget);
+      var cPassword = find.widgetWithText(TextFieldWidget, "confirm password");
+      expect(cPassword, findsOneWidget);
+      var sUp = find.widgetWithText(LongButtonWidget, "Sign Up");
+      expect(sUp, findsOneWidget);
+
+      //Now entering text into the various fields
+      await tester.enterText(name, "Alex");
+      await tester.pumpAndSettle();
+
+      await tester.enterText(email, "alex@gmail.com");
+      await tester.pumpAndSettle();
+
+      await tester.enterText(cellphone, "012345678");
+      await tester.pumpAndSettle();
+
+      await tester.enterText(location, "Hillcrest");
+      await tester.pumpAndSettle();
+
+      await tester.enterText(password, "password");
+      await tester.pumpAndSettle();
+
+      await tester.enterText(cPassword, "password");
+      await tester.pumpAndSettle();
+
+      //tap the sigup button and a pop with verify should show up
+      await tester.tap(sUp);
+      await tester.pumpAndSettle();
+
+      var description =
+          find.text("Enter verification code sent to your email/phone");
+      expect(description,
+          findsOneWidget); //this should be confirmation popup is up
+
+      //get the otp text field
+      var otp = find.widgetWithText(TextFieldWidget, "otp");
+      expect(otp, findsOneWidget);
+
+      //enter something into the otp textfield
+      await tester.enterText(otp, "12345");
+      await tester.pumpAndSettle();
+
+      //there should be a big verify button which is just checked for existence
+      expect(find.text("Verify"), findsOneWidget);
+
+      //await tester.pageBack();
+      //await tester.pumpAndSettle();
+
+      //lastly try and click that Sign In link at the bottom of the page and see
+      //if it takes you to correct page
+
+      var signIn = find.widgetWithText(GestureDetector, "Sign In");
+      expect(signIn, findsOneWidget);
+
+      //check for the other text surroundig Sign In if its correct
+      expect(find.text("Already have an account? "), findsOneWidget);
+
+      expect(find.text("or"), findsOneWidget);
     });
   });
 }
