8cd38de3dfeaac9133324156b3b35d548f05f9be
Michael du Plessis <mdp0101@gmail.com>
2022-06-07 14:58:14 +0200

    :recycle: Removed advert_id from graphql shema

--- libs/amplify/amplify/backend/function/placeBidResolver/src/index.js
+++ libs/amplify/amplify/backend/function/placeBidResolver/src/index.js
@@ -1,17 +1,28 @@
-
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+const ReverseHandTable = process.env.REVERSEHAND;
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
+*/
+
+// places a bid on an advert, requires the bid to place as well as the id the advert to place the bid
+// created_date will be made in resolver whereas UUID ofbid will be passed in
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
+    let item = {
+        TableName: ReverseHandTable,
+        Item: {
+            user_id: event.arguments.ad_id,
+            sort_key: event.arguments.bid_id, // prefixing but keeping same suffix
+            bid_details: {
+                id: event.arguments.bid_id,
+                price_lower: event.arguments.price_lower,
+                price_upper:event.arguments.price_upper,
+                quote: event.arguments.quote, //optional parameter
+                date_created: ,
+            }
+        }
     };
+
+    await docClient.put(item).promise();
 };
