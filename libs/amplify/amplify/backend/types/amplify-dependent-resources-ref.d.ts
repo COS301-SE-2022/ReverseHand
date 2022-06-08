cced7c4f275656da718496a04e061388e7db4f98
lastrucci01 <u20430168@tuks.co.za>
2022-06-09 01:14:37 +0200

    :sparkles: Created ViewJobsResolver so tradesman can view all adverts

--- libs/amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ libs/amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -59,6 +59,12 @@ export type AmplifyDependentResourcesAttributes = {
             "Arn": "string",
             "Region": "string",
             "LambdaExecutionRole": "string"
+        },
+        "viewJobsResolver": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
         }
     }
 }
