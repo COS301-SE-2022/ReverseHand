b43f9e860d4fd10c9e5069cd352a2d3f57d80746
Michael du Plessis <mdp0101@gmail.com>
2022-06-08 19:57:09 +0200

    :recycle: Removed useless MyApp widget from main

--- apps/app/lib/main.dart
+++ apps/app/lib/main.dart
@@ -1,23 +1,15 @@
 import 'package:async_redux/async_redux.dart';
 import 'package:authentication/authentication.dart';
-import 'package:general/general.dart';
 import 'package:flutter/material.dart';
 import 'package:redux_comp/redux_comp.dart';
 
 void main() async {
-  runApp(MyApp(store: Store<AppState>(initialState: AppState.initial())));
-}
-
-class MyApp extends StatelessWidget {
-  final Store<AppState> store;
-
-  const MyApp({Key? key, required this.store}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: CustomTheme.darkTheme,
-      home: LoginPage(store: store),
-    );
-  }
+  // acts as main function
+  runApp(
+    LoginPage(
+      store: Store<AppState>(
+        initialState: AppState.initial(),
+      ),
+    ),
+  );
 }
