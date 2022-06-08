11211bca4e11fd7ce87bea555e99ce0b7d77aa3f
lastrucci01 <u20430168@tuks.co.za>
2022-06-08 11:06:24 +0200

    :construction: Implemented close_advert action, not tested yet

---
+++ libs/redux_comp/lib/actions/close_advert_action.dart
@@ -0,0 +1,42 @@
+import 'package:amplify_api/amplify_api.dart';
+
+import '../app_state.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:async_redux/async_redux.dart';
+
+class CloseAdvertAction extends ReduxAction<AppState> {
+	@override
+
+  final String adId;
+  final String userId;
+
+  CloseAdvertAction(this.userId, this.adId);
+
+  Future<AppState?> reduce() async {
+    String graphQLDocument = '''mutation {
+      closeAdvert(user_id: "$userId", ad_id: "$adId") {
+        advert_id
+        user_id
+        title
+        description
+        date_created
+        date_closed
+      }
+    }''';
+
+    final request = GraphQLRequest(
+      document: graphQLDocument,
+    );
+
+    try {
+      // getting the bid which has beena accepted is just a graphql convention
+      await Amplify.API
+          .mutate(request: request)
+          .response; // in futre may want to do something with accepted advert
+
+      return state; // currently no change in state required
+    } catch (e) {
+      return state;
+    }
+  }
+}
