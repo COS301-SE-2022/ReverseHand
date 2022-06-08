02c1db35ce9b73c8209a743c4fd3c46687a46f08
Michael du Plessis <mdp0101@gmail.com>
2022-06-08 23:12:50 +0200

    :recycle: Finished QuickViewJobCardWidget

--- libs/general/lib/widgets/quick_view_job_card.dart
+++ libs/general/lib/widgets/quick_view_job_card.dart
@@ -1,14 +1,11 @@
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
+import 'package:redux_comp/actions/view_bids_action.dart';
 import 'package:redux_comp/app_state.dart';
 import 'package:redux_comp/models/advert_model.dart';
 
 class QuickViewJobCardWidget extends StatelessWidget {
-  // final String titleText;
-  // final String date;
-  // final void Function() onTap;
-
-  final AdvertModel advert;
+  final AdvertModel advert; // Current advert
   final Store<AppState> store;
 
   const QuickViewJobCardWidget({
@@ -19,58 +16,77 @@ class QuickViewJobCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => store.dispatch(ViewBidsAction(advert.id));
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (_) => ConsumerDetails(
-                store: store,
-                advert: advert,
-              ),
+    return StoreProvider<AppState>(
+      store: store,
+      child: StoreConnector<AppState, _ViewModel>(
+        vm: () => _Factory(this),
+        builder: (BuildContext context, _ViewModel vm) => InkWell(
+          onTap: () => vm.dispatchViewBidsAction(advert.id),
+          child: Card(
+            margin: const EdgeInsets.all(10),
+            color: const Color.fromRGBO(35, 47, 62, 1),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
             ),
-          );,
-      child: Card(
-        margin: const EdgeInsets.all(10),
-        color: const Color.fromRGBO(35, 47, 62, 1),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        elevation: 2,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
-          child: Row(
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                // mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(advert.title,
-                      style:
-                          const TextStyle(fontSize: 30, color: Colors.white)),
-                  const Padding(padding: EdgeInsets.all(5)),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+            elevation: 2,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+              child: Row(
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    // mainAxisSize: MainAxisSize.min,
                     children: [
-                      const Icon(
-                        Icons.access_time,
-                        color: Colors.white,
-                        size: 30.0,
-                      ),
+                      Text(advert.title,
+                          style: const TextStyle(
+                              fontSize: 30, color: Colors.white)),
                       const Padding(padding: EdgeInsets.all(5)),
-                      Column(children: [
-                        Text("Posted ${advert.dateCreated}",
-                            style: const TextStyle(
-                                fontSize: 20, color: Colors.white70)),
-                      ])
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.access_time,
+                            color: Colors.white,
+                            size: 30.0,
+                          ),
+                          const Padding(padding: EdgeInsets.all(5)),
+                          Column(children: [
+                            Text("Posted ${advert.dateCreated}",
+                                style: const TextStyle(
+                                    fontSize: 20, color: Colors.white70)),
+                          ])
+                        ],
+                      ),
                     ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ),
     );
   }
+}
+
+// factory for view model
+class _Factory extends VmFactory<AppState, QuickViewJobCardWidget> {
+  _Factory(widget) : super(widget);
+
+  @override
+  _ViewModel fromStore() => _ViewModel(
+        dispatchViewBidsAction: (adId) => dispatch(
+          ViewBidsAction(adId),
+        ),
+      );
+}
+
+// view model
+class _ViewModel extends Vm {
+  final void Function(String) dispatchViewBidsAction;
+
+  _ViewModel({
+    required this.dispatchViewBidsAction,
+  }); // implementinf hashcode
 }
