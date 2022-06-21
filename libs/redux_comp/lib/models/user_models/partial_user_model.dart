4f256cebf52fe0a434c8ca31e4a050f6e9184e3f
lastrucci01 <u20430168@tuks.co.za>
2022-06-21 16:02:06 +0200

    :alien: UserGroups implemented

--- libs/redux_comp/lib/models/user_models/partial_user_model.dart
+++ libs/redux_comp/lib/models/user_models/partial_user_model.dart
@@ -5,19 +5,19 @@ import 'package:flutter/material.dart';
 @immutable
 class PartialUser {
   final String email;
-  final String password;
+  final String group;
   final String verified;
 
-  const PartialUser(this.email, this.password, this.verified);
+  const PartialUser(this.email, this.group, this.verified);
 
   PartialUser replace({
     String? email,
-    String? password,
+    String? group,
     String? verified,
   }) {
     return PartialUser(
       email ?? this.email,
-      password ?? this.password,
+      group ?? this.group,
       verified ?? this.verified,
     );
   }
