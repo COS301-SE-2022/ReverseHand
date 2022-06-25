3b4f19bf93871d8e513c7930f2cb89bf8694926b
Michael du Plessis <mdp0101@gmail.com>
2022-06-25 15:41:02 +0200

    :sparkles: Finished resolver to change user a parameters

--- libs/amplify/amplify/backend/function/changeUserDetailsResolver/src/index.js
+++ libs/amplify/amplify/backend/function/changeUserDetailsResolver/src/index.js
@@ -1,17 +1,53 @@
+// function to change user parameters
 
+let AWS = require('aws-sdk');
 
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
-exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
+// parameters are
+// email
+// location
+// cellno
+
+// oldPass
+// newPass
+// token (the access token required to change password)
+exports.handler = (event) => {
+    let cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
+
+    // for user attributes
+    let attributes = [];
+    if (event.arguments.email !== null)
+        attributes.push({
+            Name: "email",
+            Value: event.arguments.email,
+        });
+
+    if (event.arguments.location !== null)
+        attributes.push({
+            Name: "location",
+            Value: event.arguments.location,
+        });
+
+    if (event.arguments.cellno !== null)
+        attributes.push({
+            Name: "Phone number", // fix later
+            Value: event.arguments.cellno,
+        });
+
+    let params = {
+        UserPoolId: process.env.USERPOOLID,  
+        Username: event.arguments.email,
+        UserAttributes: attributes,
     };
+
+    await cognitoIdentityServiceProvider.adminUpdateUserAttributes(params).promise();
+
+    // for password
+    if (event.arguments.token === null || event.arguments.oldPass === null || event.arguments.newPass === null)
+        return;
+
+    await cognitoIdentityServiceProvider.changePassword().promise({
+        AccessToken: event.arguments.token,
+        PreviousPassword: event.arguments.oldPass,
+        ProposedPassword: event.arguments.newPass
+    });
 };
