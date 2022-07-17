b7a49c33056fd66fb7d8d9f1b6f46a45e3d99ed4
lastrucci01 <u20430168@tuks.co.za>
2022-07-17 14:01:11 +0200

    :sparkles: Added checkUserExists action

---
+++ libs/redux_comp/lib/actions/user/check_user_exists_action.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:redux_comp/actions/user/get_user_action.dart';
+
+import '../../app_state.dart';
+import 'package:async_redux/async_redux.dart';
+
+class CheckUserExistsAction extends ReduxAction<AppState> {
+	@override
+	Future<AppState?> reduce() async {
+    String id = store.state.user!.id;
+
+    String graphQLDoc = '''query  {
+        viewUser(user_id: "$id") {
+          id        
+        }
+      }
+      ''';
+
+    final request = GraphQLRequest(
+        document: graphQLDoc,
+      );
+
+      try {
+        final data = jsonDecode(
+            (await Amplify.API.mutate(request: request).response)
+                .data);
+        final user = data["viewUser"];
+
+        if (user == "User not found") {
+          return null;
+        } else {
+          store.dispatch(GetUserAction());
+          return null;
+        }
+      } catch(e) {
+        return null;
+      }
+  }
+}
