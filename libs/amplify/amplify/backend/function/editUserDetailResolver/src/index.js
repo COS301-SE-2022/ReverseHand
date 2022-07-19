7ea4a17b9b6f3d881fa76711f2394ad63512e8d0
Alexander-Muendesi <u20426918@tuks.co.za>
2022-07-19 13:28:15 +0200

    ⚡️ Completed editUserDetailsResolver

--- libs/amplify/amplify/backend/function/editUserDetailResolver/src/index.js
+++ libs/amplify/amplify/backend/function/editUserDetailResolver/src/index.js
@@ -19,35 +19,35 @@ exports.handler = async (event) => {
             expressionAttributeNames['#domains'] = 'domains';
         }
 
-        if(event.arguments.location.address.city !== undefined){
+        if(event.arguments.city !== undefined){
             args.push('#location.address.city = :city');
             expressionAttributeNames['#location'] = 'location';
         }
 
-        if(event.arguments.location.address.street !== undefined){
+        if(event.arguments.street !== undefined){
             args.push('#location.address.street = :street');
             expressionAttributeNames['#location'] = 'location';
         }
 
-        if(event.arguments.location.address.streetNumber !== undefined)
+        if(event.arguments.streetNumber !== undefined)
         {
             args.push('#location.address.streetNumber = :streetNumber');
             expressionAttributeNames['#location'] = 'location';
         }
 
-        if(event.arguments.location.address.zipCode !== undefined)
+        if(event.arguments.zipCode !== undefined)
         {
             args.push("#location.address.zipCode = :zipCode");
             expressionAttributeNames['#location'] = 'location';
         }
 
-        if(event.arguments.location.coordinates.lat !== undefined)
+        if(event.arguments.lat !== undefined)
         {
             args.push('#location.coordinates.lat = :lat');
             expressionAttributeNames['#location'] = 'location';
         }
 
-        if(event.arguments.location.coordinates.lng !== undefined)
+        if(event.arguments.lng !== undefined)
         {
             args.push("#location.coordinates.lng = :lng");
             expressionAttributeNames['#location'] = 'location';
@@ -68,12 +68,12 @@ exports.handler = async (event) => {
 
         expressionAttributeValues[':cellNo'] = event.arguments.cellNo;
         expressionAttributeValues[':domains'] = event.arguments.domains;
-        expressionAttributeValues[':city'] = event.arguments.location.address.city;
-        expressionAttributeValues[':street'] = event.arguments.location.address.street;
-        expressionAttributeValues[':streetNumber'] = event.arguments.location.address.streetNumber;
-        expressionAttributeValues[':zipCode'] = event.arguments.location.address.zipCode;
-        expressionAttributeValues[':lat'] = event.arguments.location.coordinates.lat;
-        expressionAttributeValues[':lng'] = event.arguments.location.coordinates.lng;
+        expressionAttributeValues[':city'] = event.arguments.city;
+        expressionAttributeValues[':street'] = event.arguments.street;
+        expressionAttributeValues[':streetNumber'] = event.arguments.streetNumber;
+        expressionAttributeValues[':zipCode'] = event.arguments.zipCode;
+        expressionAttributeValues[':lat'] = event.arguments.lat;
+        expressionAttributeValues[':lng'] = event.arguments.lng;
         expressionAttributeValues[':name'] = event.arguments.name;
 
         let params = {
@@ -98,7 +98,7 @@ exports.handler = async (event) => {
 
         const data = await docClient.get(params).promise();
 
-        return data;
+        return data['Item'];
         
     } catch (error) {
         console.log(error);
