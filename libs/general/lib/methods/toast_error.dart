0bdf20084583a80e4c1fde8d70d118a0a819f50e
Michael du Plessis <mdp0101@gmail.com>
2022-07-20 00:01:47 +0200

    :sparkles: Made the toast message prettier

---
+++ libs/general/lib/methods/toast_error.dart
@@ -0,0 +1,12 @@
+// creates a toast with an error message
+import 'package:flutter/material.dart';
+import 'package:general/widgets/toast_error_widget.dart';
+
+void displayToastError(BuildContext context, String msg) {
+  SnackBar snackBar = SnackBar(
+    backgroundColor: Colors.orange,
+    content: ToastErrorWidget(msg),
+  );
+
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
---
+++ libs/general/lib/widgets/toast_error_widget.dart
@@ -0,0 +1,17 @@
+// What is held in the toast
+
+import 'package:flutter/material.dart';
+
+class ToastErrorWidget extends StatelessWidget {
+  final String msg;
+
+  const ToastErrorWidget(this.msg, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.orange,
+      child: Text(msg),
+    );
+  }
+}
