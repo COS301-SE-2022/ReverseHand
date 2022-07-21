069c5ab7f03dc023d0bdf9f49769797e7927af85
Alexander-Muendesi <u20426918@tuks.co.za>
2022-07-21 10:05:21 +0200

    ✨ Added part of editUserDetails

---
+++ libs/redux_comp/lib/actions/user/edit_user_details_action.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:redux_comp/models/geolocation/coordinates_model.dart';
+import 'package:redux_comp/models/geolocation/address_model.dart';
+import 'package:redux_comp/models/geolocation/location_model.dart';
+
+import '../../app_state.dart';
+import 'package:async_redux/async_redux.dart';
+
+class EditUserDetailsAction extends ReduxAction<AppState> {
+  final String name;
+  final String cellNo;
+  final List<String> domains; //for domains
+  final String city;
+  final String street;
+  final String streetNumber;
+  final String zipCode;
+  final String lat;
+  final String lng;
+  final String userId;
+
+  EditUserDetailsAction(
+      this.name,
+      this.cellNo,
+      this.domains,
+      this.city,
+      this.street,
+      this.streetNumber,
+      this.zipCode,
+      this.lat,
+      this.lng,
+      this.userId);
+
+  @override
+  Future<AppState?> reduce() async {
+    final d = jsonEncode(domains);
+
+    String graphQLDocument = '''mutation {
+      editUserDetail(
+        user_id: "$userId", 
+        name: "$name", 
+        cellNo: "$cellNo",
+        domains: "$d",
+        city:"$city",
+        street: "$street",
+        streetNumber:"$streetNumber",
+        zipCode: "$zipCode",
+        lat: "$lat", 
+        lng: "$lng" ){
+          cellNo
+          name
+          location {
+            coordinates {
+              lat
+              lng
+            }
+            address {
+              city
+              street
+              streetNumber
+              zipCode
+            }
+          }
+          domains
+          email
+      }
+    } ''';
+  }
+}
