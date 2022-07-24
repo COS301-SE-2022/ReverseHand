00ef25e45ba3e4823f48e0925f9edc8070752011
lastrucci01 <u20430168@tuks.co.za>
2022-07-24 14:54:38 +0200

    :twisted_rightwards_arrows: Ran amplify pull and formated the GraphQL schema

--- libs/amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ libs/amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -122,6 +122,12 @@ export type AmplifyDependentResourcesAttributes = {
             "Arn": "string",
             "Region": "string",
             "LambdaExecutionRole": "string"
+        },
+        "removeDomainResolver": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
         }
     }
 }
