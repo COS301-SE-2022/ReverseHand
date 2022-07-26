d226d3765866c739a4f850c25ebd83084e59d639
lastrucci01 <u20430168@tuks.co.za>
2022-07-26 15:48:58 +0200

    :twisted_rightwards_arrows: Ran amplify pull

---
+++ libs/amplify/amplify/backend/function/getConsumerMessagesResolver/src/index.js
@@ -0,0 +1,23 @@
+// resolver that gets all messages for a consumer
+// requires a consumer id
+
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+const ReverseHandTable = process.env.REVERSEHAND;
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+    let params = {
+        TableName: ReverseHandTable,
+        KeyConditionExpression: "part_key = :c",
+        ExpressionAttributeValues: {
+            ":c": event.arguments.c_id, // should be a consumers id
+        }
+    };
+
+    const data = await docClient.query(params).promise();
+
+    return data.Items;
+};
