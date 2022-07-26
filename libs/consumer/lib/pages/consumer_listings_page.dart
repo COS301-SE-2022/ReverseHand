8ef9fc0d099f5e9795550cc3ff2106eea5ae50da
Michael du Plessis <mdp0101@gmail.com>
2022-07-27 00:00:10 +0200

    :sparkles: editAdvert action now throws an error if the advert contains bids

--- libs/consumer/lib/pages/consumer_listings_page.dart
+++ libs/consumer/lib/pages/consumer_listings_page.dart
@@ -1,11 +1,6 @@
 import 'package:async_redux/async_redux.dart';
-
 import 'package:flutter/material.dart';
 import 'package:general/general.dart';
-import 'package:general/widgets/divider.dart';
-import 'package:general/widgets/skeleton_loader.dart';
-import 'package:general/widgets/test.dart';
-
 import 'package:redux_comp/models/advert_model.dart';
 import 'package:redux_comp/redux_comp.dart';
 import '../methods/populate_adverts.dart';
