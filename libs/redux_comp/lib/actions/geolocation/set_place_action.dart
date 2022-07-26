518318a5b254ed62ff06e50b8870571ff32cc653
lastrucci01 <u20430168@tuks.co.za>
2022-07-26 11:25:11 +0200

    :recycle: Attempting to fix domains bug

---
+++ libs/redux_comp/lib/actions/geolocation/set_place_action.dart
@@ -0,0 +1,42 @@
+import 'package:redux_comp/models/error_type_model.dart';
+import 'package:redux_comp/models/geolocation/coordinates_model.dart';
+import 'package:redux_comp/models/geolocation/domain_model.dart';
+import '../../app_state.dart';
+import 'package:async_redux/async_redux.dart';
+
+class SetPlaceAction extends ReduxAction<AppState> {
+
+  
+	@override
+	Future<AppState?> reduce() async {
+    if (state.locationResult != null) {
+      switch (state.userDetails!.userType) {
+        case "Consumer":
+          return state.copy(
+            userDetails: state.userDetails!.copy(location: state.locationResult),
+          );
+        case "Tradesman":
+          List<Domain> userDomains = state.userDetails!.domains;
+          userDomains.add(Domain(
+            city: state.locationResult!.address.city,
+            coordinates: Coordinates(
+                lat: state.locationResult!.coordinates.lat, lng: state.locationResult!.coordinates.lng),
+          ));
+          return state.copy(
+            userDetails: state.userDetails!.copy(domains: userDomains,location: null),
+          );
+        default:
+        return null;
+      }
+    } else {
+      return state.copy(
+        error: ErrorType.locationNotCaptured
+      );
+    }
+  }
+
+  @override
+  void after() {
+   dispatch(NavigateAction.pop());
+  }
+}
