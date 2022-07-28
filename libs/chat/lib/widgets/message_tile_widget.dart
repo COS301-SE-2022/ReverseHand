81eb31d00b82d006d4991096cd5a0fdf94432847
Michael du Plessis <mdp0101@gmail.com>
2022-07-28 21:12:32 +0200

    :hankey: Should not push from develop

--- libs/chat/lib/widgets/message_tile_widget.dart
+++ libs/chat/lib/widgets/message_tile_widget.dart
@@ -103,7 +103,7 @@ String getTime(int timestamp) {
   DateTime date = DateTime.fromMillisecondsSinceEpoch(timestamp.toInt());
   String minute = date.minute.toString();
   minute = minute.length == 1 ? "0$minute" : minute;
-  String hour = date.minute.toString();
+  String hour = date.hour.toString();
   hour = hour.length == 1 ? "0$hour" : hour;
 
   return "$hour:$minute";
