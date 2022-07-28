fe791bd6101294adb449546d4407232caeaee1fe
Michael du Plessis <mdp0101@gmail.com>
2022-07-28 18:20:17 +0200

    :sparkles: Are able to now send chat messages from app

--- libs/redux_comp/lib/actions/chat/send_message_action.dart
+++ libs/redux_comp/lib/actions/chat/send_message_action.dart
@@ -12,7 +12,7 @@ class SendMessageAction extends ReduxAction<AppState> {
   @override
   Future<AppState?> reduce() async {
     String graphQLDocument = '''mutation {
-      sendMessage(c_id: "${state.chat.consumerId}", t_id: "${state.chat.tradesmanId}", msg: $msg, sender: ${state.userDetails!.userType.toLowerCase()}, name: ${state.userDetails!.name}) {
+      sendMessage(c_id: "${state.chat.consumerId}", t_id: "${state.chat.tradesmanId}", msg: "$msg", sender: "${state.userDetails!.userType.toLowerCase()}", name: "${state.userDetails!.name}") {
         consumer_id
         tradesman_id
         msg
