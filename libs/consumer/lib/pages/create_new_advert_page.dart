4113971330091374f7f0264d775b2b8b4f8c3177
lastrucci01 <u20430168@tuks.co.za>
2022-07-28 18:24:22 +0200

    :sparkles: Integrated Consumer Radio button for create_advert

--- libs/consumer/lib/pages/create_new_advert_page.dart
+++ libs/consumer/lib/pages/create_new_advert_page.dart
@@ -22,9 +22,9 @@ class CreateNewAdvertPage extends StatefulWidget {
 
 class _CreateNewAdvertPageState extends State<CreateNewAdvertPage> {
   final titleController = TextEditingController();
-  var tradeController = TextEditingController();
   final descrController = TextEditingController();
 
+  String? trade;
 
   void showRadioSelect() async {
     final String? result = await showDialog(
@@ -37,7 +37,7 @@ class _CreateNewAdvertPageState extends State<CreateNewAdvertPage> {
     // Update UI
     if (result != null) {
       setState(() {
-        tradeController = TextEditingController(text: result);
+        trade = result;
       });
     }
   }
@@ -45,7 +45,6 @@ class _CreateNewAdvertPageState extends State<CreateNewAdvertPage> {
   @override
   void dispose() {
     titleController.dispose();
-    tradeController.dispose();
     descrController.dispose();
     super.dispose();
   }
@@ -122,12 +121,13 @@ class _CreateNewAdvertPageState extends State<CreateNewAdvertPage> {
                           : ButtonWidget(
                               text: "Create Job",
                               function: () {
-                                if (titleController.value.text != "") {
+                                if (titleController.value.text != "" && trade != null) {
                                   vm.dispatchCreateAdvertActions(
                                       widget.store.state.userDetails!.id,
                                       titleController.value.text,
                                       widget.store.state.userDetails!.location!.address
                                           .city,
+                                      trade!,
                                       descrController.value.text);
                                 }
                               }),
@@ -163,13 +163,13 @@ class _Factory extends VmFactory<AppState, _CreateNewAdvertPageState> {
   _ViewModel fromStore() => _ViewModel(
         popPage: () => dispatch(NavigateAction.pop()),
         dispatchCreateAdvertActions: (String customerId, String title,
-                String location, String? description) =>
+                String location, String trade, String? description) =>
             dispatch(
           CreateAdvertAction(
             customerId,
             title,
             location,
-            "Plumbing",
+            trade,
             description: description,
           ),
         ),
@@ -179,7 +179,7 @@ class _Factory extends VmFactory<AppState, _CreateNewAdvertPageState> {
 
 // view model
 class _ViewModel extends Vm {
-  final void Function(String, String, String, String?)
+  final void Function(String id, String title, String location, String trade, String? descr)
       dispatchCreateAdvertActions;
   final VoidCallback popPage;
   final bool loading;
