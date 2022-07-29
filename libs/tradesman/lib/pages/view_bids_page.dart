7c41d9ec8e2503a99b2447c62f4eca49cf42ed91
lastrucci01 <u20430168@tuks.co.za>
2022-07-29 08:29:26 +0200

    :bug: Check if tradetypes in not empty in addition to null check

--- libs/tradesman/lib/pages/view_bids_page.dart
+++ libs/tradesman/lib/pages/view_bids_page.dart
@@ -48,7 +48,7 @@ class TradesmanViewBidsPage extends StatelessWidget {
                     titleText: vm.advert.title,
                     descText: vm.advert.description ?? "",
                     date: vm.advert.dateCreated,
-                    type: vm.advert.type!,
+                    type: vm.advert.type ?? "",
                     location: vm.advert.location,
                   ),
                   //*******************************************//
