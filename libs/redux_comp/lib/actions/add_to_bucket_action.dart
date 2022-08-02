493fe96cbda3524ae315cf93eea9f8cea182524a
Michael du Plessis <mdp0101@gmail.com>
2022-08-02 12:23:34 +0200

    :recycle: Finished action to upload file to s3

--- libs/redux_comp/lib/actions/add_to_bucket_action.dart
+++ libs/redux_comp/lib/actions/add_to_bucket_action.dart
@@ -1,19 +1,52 @@
 import 'dart:io';
 import 'package:amplify_storage_s3/amplify_storage_s3.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
 import '../app_state.dart';
 import 'package:async_redux/async_redux.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 
+// enum to show what kind of upload
+enum FileType {
+  profile,
+  job,
+  quote,
+}
+
+// Action to upload files to an s3 bucket
 class AddToBucketAction extends ReduxAction<AppState> {
+  final FileType fileType; // type of file
+  final File file; // file to upload
+  final String? advertId;
+
+  AddToBucketAction(
+    this.fileType,
+    this.file, {
+    this.advertId,
+  });
+
   @override
   Future<AppState?> reduce() async {
-    // Create a dummy file
-    const exampleString = 'Example file contents';
-    final tempDir = await getTemporaryDirectory();
-    final exampleFile = File('${tempDir.path}/example.txt')
-      ..createSync()
-      ..writeAsStringSync(exampleString);
+    final String key;
+
+    switch (fileType) {
+      case FileType.profile:
+        key = "profile";
+        break;
+      case FileType.job: // happens if consumer is logged in
+        if (advertId == null) {
+          throw "Advert ID cannot be null if FileType is job.";
+        }
+        String uuid = const Uuid().v1(); // unique id of image
+        key = "$advertId!/$uuid";
+        break;
+      case FileType.quote: // happens when tradesman is logged in
+        if (advertId == null) {
+          throw "Advert ID cannot be null if FileType is quote.";
+        }
+        key = "quotes/$advertId!";
+        break;
+    }
 
     // Set the access level to `protected` for the current user
     // Note: A user must be logged in through Cognito Auth
@@ -27,8 +60,8 @@ class AddToBucketAction extends ReduxAction<AppState> {
     // Upload the file to S3 with protected access
     try {
       final UploadFileResult result = await Amplify.Storage.uploadFile(
-          local: exampleFile,
-          key: 'ExampleKey',
+          local: file,
+          key: key,
           options: uploadOptions,
           onProgress: (progress) {
             print('Fraction completed: ${progress.getFractionCompleted()}');
