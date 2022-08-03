e4fddc62b2cfd32bb1f83cfa19741cdf4117ff9c
arlo216 <charlottevzyl1@gmail.com>
2022-08-03 17:27:31 +0200

    💄Updating tradesman profile to new designs, temporary old profile file created

--- libs/tradesman/lib/pages/tradesman_profile_page.dart
+++ libs/tradesman/lib/pages/tradesman_profile_page.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:general/general.dart';
 import 'package:redux_comp/models/user_models/user_model.dart';
 import 'package:redux_comp/redux_comp.dart';
-import 'package:general/widgets/appbar.dart';
-import 'package:general/widgets/bottom_overlay.dart';
+import 'package:general/widgets/profile_divider.dart';
 
 import '../widgets/navbar.dart';
 
@@ -22,185 +21,109 @@ class TradesmanProfilePage extends StatelessWidget {
           body: SingleChildScrollView(
             child: StoreConnector<AppState, _ViewModel>(
               vm: () => _Factory(this),
-              builder: (BuildContext context, _ViewModel vm) => Column(
-                children: [
-                  //*******************APP BAR WIDGET*********************//
-                  AppBarWidget(title: "PROFILE", store: store),
-                  //********************************************************//
-
-                  //*******************CONSUMER NAME************************//
-                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                    const Icon(
-                      Icons.account_circle,
-                      color: Colors.white,
-                      size: 50.0,
+              builder: (BuildContext context, _ViewModel vm) =>
+                  Column(children: [
+                //**************HEADING***************/
+                Padding(
+                  padding: const EdgeInsets.only(top: 80),
+                  child: Center(
+                    child: Text(
+                      (vm.userDetails.name != null)
+                          ? vm.userDetails.name!
+                          : "null",
+                      style: const TextStyle(fontSize: 35),
                     ),
-                    const Padding(padding: EdgeInsets.only(right: 10)),
-                    Text(
-                        (vm.userDetails.name != null) ? vm.userDetails.name! : "null",
-                      style: const TextStyle(fontSize: 30),
-                    ),
-                  ]),
-                  //********************************************************//
+                  ),
+                ),
+                //************************************/
 
-                  const Padding(padding: EdgeInsets.all(20)),
+                //****************ICON****************/
+                const Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Icon(
+                    Icons.account_circle,
+                    color: Colors.white,
+                    size: 80.0,
+                  ),
+                ),
+                //************************************/
 
-                  //*******************CONSUMER DETAILS************************//
-
-                  Stack(alignment: Alignment.center, children: <Widget>[
-                    //overlay
-                    BottomOverlayWidget(
-                      height: MediaQuery.of(context).size.height / 1.5,
-                    ),
-                    //email
-                    Positioned(
-                      top: 40,
-                      left: 45,
-                      child: Row(
-                        children: const [
-                          Icon(
-                            Icons.email,
-                            color: Colors.white70,
-                            size: 26.0,
-                          ),
-                          Padding(padding: EdgeInsets.only(left: 8)),
-                          Text("Email",
-                              style: TextStyle(
-                                  fontSize: 26, color: Colors.white70)),
-                        ],
-                      ),
-                    ),
-
-                    Positioned(
-                        top: 80,
-                        left: 52,
-                        child: Text(vm.userDetails.email,
-                            style: const TextStyle(
-                                fontSize: 20, color: Colors.white))),
-                    //********************************************************//
-
-                    //cellphone
-                    Positioned(
-                      top: 140,
-                      left: 45,
-                      child: Row(
-                        children: const [
-                          Icon(
-                            Icons.phone,
-                            color: Colors.white70,
-                            size: 26.0,
-                          ),
-                          Padding(padding: EdgeInsets.only(left: 8)),
-                          Text("Phone",
-                              style: TextStyle(
-                                  fontSize: 26, color: Colors.white70)),
-                        ],
-                      ),
-                    ),
-
-                    Positioned(
-                        top: 180,
-                        left: 52,
-                        child: Text((vm.userDetails.cellNo != null) ? vm.userDetails.cellNo! : "null",
-                            style: const TextStyle(
-                                fontSize: 20, color: Colors.white))),
-                    //********************************************************//
-
-                    //Trade
-                    Positioned(
-                      top: 240,
-                      left: 45,
-                      child: Row(
-                        children: const [
-                          Icon(
-                            Icons.construction_outlined,
-                            color: Colors.white70,
-                            size: 26.0,
-                          ),
-                          Padding(padding: EdgeInsets.only(left: 8)),
-                          Text("Trade",
-                              style: TextStyle(
-                                  fontSize: 26, color: Colors.white70)),
-                        ],
-                      ),
-                    ),
-
-                    // to be integrated by Richard,
-                    // ...more like badly integrated by Richard
-                    Positioned(
-                      top: 265,
-                      left: 52,
-                      child: SizedBox(
-                        width: 200,
-                        height: 50,
-                        child: ListView(
-                          children: vm.userDetails.tradeTypes
-                              .map((element) => Text(element,
-                                  style: const TextStyle(
-                                      fontSize: 20, color: Colors.white)))
-                              .toList(),
-                        ),
-                      ),
-                    ),
-
-                    //********************************************************//
-
-                    //domains
-                    Positioned(
-                      top: 340,
-                      left: 45,
-                      child: Row(
-                        children: const [
-                          Icon(
-                            Icons.location_on,
-                            color: Colors.white70,
-                            size: 26.0,
-                          ),
-                          Padding(padding: EdgeInsets.only(left: 8)),
-                          Text("Domains",
-                              style: TextStyle(
-                                  fontSize: 26, color: Colors.white70)),
-                        ],
-                      ),
-                    ),
-
-                    Positioned(
-                        top: 370,
-                        left: 52,
-                        child: SizedBox(
-                          width: 200,
-                        height: 50,
-                        child: ListView(
-                          children: vm.userDetails.domains
-                              .map((domain) => Text(domain.city,
-                                  style: const TextStyle(
-                                      fontSize: 20, color: Colors.white)))
-                              .toList(),
-                        ),
-                        )),
-                    //********************************************************//
-
-                    Positioned(
-                      top: 10,
-                      right: 35,
-                      child: IconButton(
-                        onPressed: vm.pushEditProfilePage,
-                        icon: const Icon(Icons.edit),
+                //************NAME*******************/
+                Padding(
+                  padding: const EdgeInsets.only(left: 110, top: 40),
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.person,
                         color: Colors.white70,
+                        size: 26.0,
                       ),
-                    ),
-                  ]),
-                ],
-              ),
+                      const Padding(padding: EdgeInsets.only(right: 5)),
+                      Text(
+                        (vm.userDetails.name != null)
+                            ? vm.userDetails.name!
+                            : "null",
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                ),
+                //************************************/
+
+                const ProfileDividerWidget(),
+
+                //************CELL*******************/
+                Padding(
+                  padding: const EdgeInsets.only(left: 110, top: 5),
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.phone,
+                        color: Colors.white70,
+                        size: 26.0,
+                      ),
+                      const Padding(padding: EdgeInsets.only(right: 5)),
+                      Text(
+                          (vm.userDetails.cellNo != null)
+                              ? vm.userDetails.cellNo!
+                              : "null",
+                          style: const TextStyle(
+                              fontSize: 20, color: Colors.white)),
+                    ],
+                  ),
+                ),
+                //************************************/
+
+                const ProfileDividerWidget(),
+
+                //************EMAIl*******************/
+                Padding(
+                  padding: const EdgeInsets.only(left: 110, top: 5),
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.email,
+                        color: Colors.white70,
+                        size: 26.0,
+                      ),
+                      const Padding(padding: EdgeInsets.only(right: 5)),
+                      Text(vm.userDetails.email,
+                          style: const TextStyle(
+                              fontSize: 20, color: Colors.white)),
+                    ],
+                  ),
+                ),
+                //************************************/
+
+                const ProfileDividerWidget()
+              ]),
             ),
           ),
-
-          //************************NAVBAR***********************/
+          //****************NAVBAR****************/
           bottomNavigationBar: TNavBarWidget(
             store: store,
           ),
-
-          //*************************************************//
+          //**************************************/
         ),
       ),
     );
@@ -221,7 +144,7 @@ class _Factory extends VmFactory<AppState, TradesmanProfilePage> {
 
 // view model
 class _ViewModel extends Vm {
- final UserModel userDetails;
+  final UserModel userDetails;
   final VoidCallback pushEditProfilePage;
 
   _ViewModel({
