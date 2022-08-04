917f83cd886ca1ef7e22c58442103bd412936920
arlo216 <charlottevzyl1@gmail.com>
2022-08-04 12:36:08 +0200

    ♻️ removing unused imports and variables

--- libs/redux_comp/lib/actions/intiate_auth_action.dart
+++ libs/redux_comp/lib/actions/intiate_auth_action.dart
@@ -5,15 +5,13 @@ import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:async_redux/async_redux.dart';
 
 class IntiateAuthAction extends ReduxAction<AppState> {
-	@override
-	Future<AppState?> reduce() async {
+  @override
+  Future<AppState?> reduce() async {
     final authSession = await Amplify.Auth.fetchAuthSession();
     if (authSession.isSignedIn == true) {
-
+      return null;
     } else {
-      return state.copy(
-        error: ErrorType.userNotAuthorised
-      );
+      return state.copy(error: ErrorType.userNotAuthorised);
     }
   }
 }
--- libs/redux_comp/lib/actions/user/signin_facebook_action.dart
+++ libs/redux_comp/lib/actions/user/signin_facebook_action.dart
@@ -1,39 +1,37 @@
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:flutter/material.dart';
-import 'package:jwt_decode/jwt_decode.dart';
+// import 'package:jwt_decode/jwt_decode.dart';
 
 import '../../app_state.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:async_redux/async_redux.dart';
 
 class SigninFacebookAction extends ReduxAction<AppState> {
-
   SigninFacebookAction();
 
   @override
   Future<AppState?> reduce() async {
     try {
       // Amplify.Auth.signOut();
-      final result =
-          await Amplify.Auth.signInWithWebUI(provider: AuthProvider.google);
+      // final result =
+      await Amplify.Auth.signInWithWebUI(provider: AuthProvider.google);
 
-      String id="", userType="", email ="";
-      final info = await Amplify.Auth.fetchUserAttributes(); 
-      final authSession = (await Amplify.Auth.fetchAuthSession(
-        options: CognitoSessionOptions(getAWSCredentials: true),
-      )) as CognitoAuthSession;
+      // String id="", userType="", email ="";
+      final info = await Amplify.Auth.fetchUserAttributes();
+      // final authSession = (await Amplify.Auth.fetchAuthSession(
+      // options: CognitoSessionOptions(getAWSCredentials: true),
+      // )) as CognitoAuthSession;
 
-      Map<String, dynamic> payload =
-          Jwt.parseJwt(authSession.userPoolTokens!.accessToken);
+      // Map<String, dynamic> payload =
+      //     Jwt.parseJwt(authSession.userPoolTokens!.accessToken);
 
-      List groups = await payload["cognito:groups"];
+      // List groups = await payload["cognito:groups"];
 
-      if (groups.contains("tradesman")) {
-        userType = "Tradesman";
-      } else if (groups.contains("customer")) {
-        userType = "Consumer";
-      }
-
+      // if (groups.contains("tradesman")) {
+      //   userType = "Tradesman";
+      // } else if (groups.contains("customer")) {
+      //   userType = "Consumer";
+      // }
 
       debugPrint(info.toString());
       return null;
