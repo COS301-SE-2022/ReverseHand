6d5344955ceaf6a33b68b10bae5a6668ae7fd73b
lastrucci01 <u20430168@tuks.co.za>
2022-08-05 13:02:58 +0200

    :arrow_up: Ran pod upgrade & install, created new reducer

---
+++ libs/redux_comp/lib/actions/refresh_user_token_action.dart
@@ -0,0 +1,12 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+
+import '../app_state.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:async_redux/async_redux.dart';
+
+class RefreshUserTokenAction extends ReduxAction<AppState> {
+	@override
+	Future<AppState?> reduce() async {
+    
+  }
+}
