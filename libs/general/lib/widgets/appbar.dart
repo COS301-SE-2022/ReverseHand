c3e807ee9b09bc22955037cba25a347ff0e7dcac
caaaramia <u20478144@tuks.co.za>
2022-08-12 14:56:22 +0200

    💄 Refactored appbar to be squared off

--- libs/general/lib/widgets/appbar.dart
+++ libs/general/lib/widgets/appbar.dart
@@ -12,64 +12,39 @@ class AppBarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //ClipPath is alternative - keep this comment for now to remember
     return StoreProvider<AppState>(
         store: store,
         child: StoreConnector<AppState, _ViewModel>(
           vm: () => _Factory(this),
-          builder: (BuildContext context, _ViewModel vm) => PhysicalShape(
-            clipper: AppBarClipper(),
-            elevation: 3.0,
-            color: Theme.of(context).primaryColorDark,
-            child: Container(
-              height: 155,
-              alignment: const Alignment(0, -.5),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(22),
-                    child: Text(
-                      title,
-                      style: const TextStyle(fontSize: 20),
-                    ),
-                  ),
-                  IconButton(
-                    icon: const Icon(
-                      Icons.notifications,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {
-                      vm.pushActivityStreamPage();
-                    },
-                    splashRadius: 1,
-                    // highlightColor: Colors.orange,
-                    // splashColor: Colors.white,
-                  ),
-                ],
+          builder: (BuildContext context, _ViewModel vm) => 
+          AppBar(
+            title: Text(title),
+            backgroundColor: Theme.of(context).primaryColorDark,
+            leading: IconButton(
+              icon: const Icon(
+                Icons.notifications,
+                color: Colors.white,
               ),
+              onPressed: () {
+                vm.pushActivityStreamPage();
+              },
+              splashRadius: 1,
+              highlightColor: Colors.orange,
+              splashColor: Colors.white,
             ),
+            actions: [
+              Container(
+                width: 50,
+                child: Image.asset( 
+                   'assets/images/logo.png',
+                    package: 'authentication',
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(right: 10)),
+            ],
           ),
         ));
   }
-}
-
-class AppBarClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    double curveHeight = size.height / 2;
-    var p = Path()
-      ..lineTo(0, size.height)
-      ..quadraticBezierTo(0, curveHeight, curveHeight, curveHeight)
-      ..lineTo(size.width - curveHeight, curveHeight)
-      ..quadraticBezierTo(size.width, curveHeight, size.width, size.height)
-      ..lineTo(size.width, 0);
-
-    return p;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
 }
 
 // factory for view model
