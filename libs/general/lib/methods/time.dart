9427ec0c9ccbbf03c21f8d15fcab8f48b46c2ad8
Michael du Plessis <mdp0101@gmail.com>
2022-08-15 11:28:14 +0200

    :recycle: made date appear liek picky Richard wants

--- libs/general/lib/methods/time.dart
+++ libs/general/lib/methods/time.dart
@@ -1,5 +1,20 @@
+// List<String> _months = [
+//   "January",
+//   "Febuary",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "December",
+// ];
+
 String timestampToDate(double timestamp) {
   DateTime date = DateTime.fromMillisecondsSinceEpoch(timestamp.toInt());
-
-  return "${date.month}-${date.day}";
+  String d = date.toString();
+  return "${d.substring(8, 10)}-${d.substring(5, 7)}-${d.substring(0, 4)}";
 }
