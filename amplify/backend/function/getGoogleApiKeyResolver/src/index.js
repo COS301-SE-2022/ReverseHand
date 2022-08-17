bf57156972303dab4bd3fc9e564e8ab2b6155505
caaaramia <u20478144@tuks.co.za>
2022-08-17 11:04:27 +0200

    🔥 🙈 Removed amplify library from main app folder structure from accidental amplify pull

--- amplify/backend/function/getGoogleApiKeyResolver/src/index.js
+++
@@ -1,6 +0,0 @@
-const SecretsManager = require('./secretsmanager.js');
-
-exports.handler = async (event) => {
-    let apiKey = await SecretsManager.getSecret("GoogleApiKey", "eu-west-1");
-    return JSON.parse(apiKey);
-};
