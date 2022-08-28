fda72f3b2c92dad2a057fdc14f02607278428d13
caaaramia <u20478144@tuks.co.za>
2022-08-28 23:27:34 +0200

    💄 Updated UI on camera_preview page

--- libs/general/lib/pages/camera_preview_page.dart
+++ libs/general/lib/pages/camera_preview_page.dart
@@ -2,6 +2,8 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 
+import '../widgets/button.dart';
+
 //************************************ */
 //  Photo preview page after taking on
 //  live camera page
@@ -15,13 +17,25 @@ class PreviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Preview Page')),
+      appBar: AppBar(
+          title: const Text('Preview Image'),
+          backgroundColor: const Color.fromRGBO(243, 157, 55, 1),
+        ),
       body: Center(
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Image.file(File(picture.path), fit: BoxFit.cover, width: 250),
-          const SizedBox(height: 24),
-          Text(picture.name)
-        ]),
+        child: Column(
+          mainAxisSize: MainAxisSize.min, children: [
+            Image.file(File(picture.path), fit: BoxFit.cover, width: 250),
+            const SizedBox(height: 24),
+            Text(picture.name),
+            Positioned(
+                top: 35,
+                child: ButtonWidget(
+                    text: "Choose Photo",
+                    function: () {
+                    
+                    })),
+          ]
+        ),
       ),
     );
   }
