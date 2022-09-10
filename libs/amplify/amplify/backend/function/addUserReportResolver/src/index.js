7d837e5e25d916c38800d33e0e1fee972d8a6c6e
lastrucci01 <u20430168@tuks.co.za>
2022-09-11 00:44:05 +0200

    :recycle: Refactored add & remove resolvers for user reports & review reports

--- libs/amplify/amplify/backend/function/addUserReportResolver/src/index.js
+++ libs/amplify/amplify/backend/function/addUserReportResolver/src/index.js
@@ -1,52 +1,26 @@
 const AWS = require("aws-sdk");
 const docClient = new AWS.DynamoDB.DocumentClient();
-const UserTable = process.env.USER;
-
+const ReverseHandTable = process.env.REVERSEHAND;
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-   
-   let params = {
-      TableName: UserTable,
-      ReturnValues: 'ALL_NEW',
-      Key: {
-        user_id: event.arguments.user_id,
-      },
-      UpdateExpression: `set user_reports = list_append(if_not_exists(user_reports,:list),:report)`,
-      ExpressionAttributeValues: {
-        ":report": [event.arguments.report],
-        ":list" : []
-      },
-    };
-    
-    const user = await docClient.update(params).promise().then((resp) => resp.Attributes);
-    
-    params = {
-      TableName: UserTable,
-      Key: {
-        user_id: "reported#users",
-      },
-    };
-    
-    const user_reports_list = await docClient.get(params).promise().then((resp) => resp.Item);
-    if (event.arguments.user_id[0] == "c") {
-      if (!user_reports_list.customers.some(e => e.user_id === event.arguments.user_id)) {
-        user_reports_list.customers.push({"user_id": event.arguments.user_id});
+  
+  event.arguments.report.reporter_id = event.arguments.user_id;
+  let paramsPutReport = {
+    TableName: ReverseHandTable,
+    Item: {
+      part_key: "user_reports#" + event.arguments.report.reported_user_id,
+      sort_key: "report#" + AWS.util.uuid.v4(),
+      report_details: event.arguments.report,
+      report_type: "user#reports"
     }
-    } else {
-      if (!user_reports_list.tradesmen.some(e => e.user_id === event.arguments.user_id)) {
-        user_reports_list.tradesmen.push({"user_id": event.arguments.user_id});
-      }
-    }
-    
-    params = {
-      TableName: UserTable,
-      Item: user_reports_list
-    };
-    
-    await docClient.put(params).promise();
-    
-    return event.arguments.report;
+  };
+
+  await docClient.put(paramsPutReport).promise();
+  
+  paramsPutReport.Item.report_details.id = paramsPutReport.Item.sort_key;
+  delete paramsPutReport.Item.sort_key;
+  return paramsPutReport.Item.report_details;
 };
