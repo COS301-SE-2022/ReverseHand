15e1c8f18bb7ef8991a7ed0a7c93cda6395503ec
lastrucci01 <u20430168@tuks.co.za>
2022-09-10 12:14:23 +0200

    :construction: GetMetricStatistics resolver updated

--- libs/amplify/amplify/backend/function/getSystemMetricsResolver/src/index.js
+++ libs/amplify/amplify/backend/function/getSystemMetricsResolver/src/index.js
@@ -6,19 +6,32 @@ AWS.config.update({ region: 'eu-west-1' });
  */
 exports.handler = async (event) => {
     let cloudWatch = new AWS.CloudWatch({ apiVersion: '2010-08-01' });
-
+    var date = new Date();
+    console.log((date-7 * 24 * 60 * 60 * 1000));
     let params = {
-        'Dimensions': [
+        EndTime: new Date(date.getTime()), /* required */
+        MetricName: 'ConsumedReadCapacityUnits', /* required */
+        Namespace: 'AWS/DynamoDB', /* required */
+        Period: '10', /* required */
+        StartTime: new Date((date-7 * 24 * 60 * 60 * 1000)), /* required */
+        Dimensions: [
             {
-                'Name': 'TableName', /* required */
-                'Value': 'ReverseHand'
+                Name: 'TableName', /* required */
+                Value: 'ReverseHand' /* required */
             },
             /* more items */
         ],
-        'MetricName': 'ProvisionedWriteCapacityUnits',
-        'Namespace': 'AWS/DynamoDB',
+        // ExtendedStatistics: [
+        //   'STRING_VALUE',
+        //   /* more items */
+        // ],
+        Statistics: [
+            "Average",
+            /* more items */
+        ],
+        Unit: "Seconds"
     };
 
-    
-    return await cloudWatch.listMetrics(params).promise();
+
+    // return await cloudWatch.getMetricStatistics(params).promise();
 }
