0aa50db00ec2dff4d051a055018494fbcddf8022
caaaramia <u20478144@tuks.co.za>
2022-09-13 12:57:42 +0200

    💄 Fixed login company logos

--- libs/consumer/lib/pages/advert_details_page.dart
+++ libs/consumer/lib/pages/advert_details_page.dart
@@ -26,9 +26,7 @@ class AdvertDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     //this are where we do the images
     final List<String> images = [
-        "https://media.istockphoto.com/photos/mess-and-dump-an-old-room-with-lots-of-things-devastation-very-small-picture-id1189357377?k=20&m=1189357377&s=612x612&w=0&h=l2VJRihipV0DSRf2VImuCde4wloj4vkuJhylLWcybC8=",
-        "https://www.researchgate.net/publication/264635711/figure/fig2/AS:213433816490008@1427897995893/Living-room-The-patients-living-room-was-filled-with-dirty-clothing-old-newspaper-and.png",
-        "https://renegademothering.com/wp-content/uploads/2015/06/FullSizeRender-5.jpg",
+       "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
 
     ];
 
