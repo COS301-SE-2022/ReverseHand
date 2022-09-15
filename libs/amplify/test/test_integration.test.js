e6b67852b56c3b6728c23e32bbdc860ac6401a0e
Alexander-Muendesi <u20426918@tuks.co.za>
2022-09-15 23:29:45 +0200

    🐛 adding console.log to see potential error

--- libs/amplify/test/test_integration.test.js
+++ libs/amplify/test/test_integration.test.js
@@ -1,5 +1,5 @@
 const AWS = require("aws-sdk");
-require('dotenv').config();
+console.log(require('dotenv').config());
 const event = {
     arguments: {
         user_id : "c#f0fa74ae-7a4d-4674-8ba6-e9da6f6213cd"   
