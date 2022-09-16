81c6298fb401dfeb994d944a315bd15f121f0dda
Alexander-Muendesi <u20426918@tuks.co.za>
2022-09-16 12:37:53 +0200

    ✅ Integration Test: CreateAdvertResolver

---
+++ libs/amplify/test/1_createAdvertResolver.test.js
@@ -0,0 +1,44 @@
+const AWS = require("aws-sdk");
+require('dotenv').config();
+
+const event = {
+    arguments: {
+        customer_id : "c#f0fa74ae-7a4d-4674-8ba6-e9da6f6213cd",
+        title: "Lambda Test One",
+        description: "This is a test description",
+        domain :{
+            city: "Pretoria",
+            province: "Gauteng",
+            coordinates : {
+                lng: 28.2625799,
+                lat: -25.7670289
+            }
+        },
+        type: "Plumbing",
+    }
+};
+
+jest.setTimeout(1000000);
+
+describe("Create Advert Resolver", () =>{
+    beforeAll(() => {
+        process.env.REVERSEHAND = 'ReverseHand'; 
+        AWS.config.update({
+            region: process.env.REGION,
+            credentials: {
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            }
+        });
+    });
+
+    test("Testing Advert Details from advert that was created", async () =>{
+        const handlerModule = require('../amplify/backend/function/createAdvertResolver/src/index');
+        const result = await handlerModule.handler(event);
+
+        expect(result.title).toEqual('Lambda Test One');
+        expect(result.description).toEqual('This is a test description');
+        expect(result.type).toEqual('Plumbing');
+
+    })
+});
