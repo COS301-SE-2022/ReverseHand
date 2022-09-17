0175c0bd99f2ecd0d5f90333941d51e5a76b4711
caaaramia <u20478144@tuks.co.za>
2022-09-17 20:33:49 +0200

    ✨ Implemented display success toast method

--- libs/general/lib/methods/toast_success.dart
+++ libs/general/lib/methods/toast_success.dart
@@ -1,13 +1,13 @@
-// // creates a toast with an error message
-// import 'package:flutter/material.dart';
-// import 'package:general/widgets/toast_error_widget.dart';
+// creates a toast with an error message
+import 'package:flutter/material.dart';
+import 'package:general/widgets/toast_success_widget.dart';
 
-// void displayToastSuccess(BuildContext context, String msg) {
-//   SnackBar snackBar = SnackBar(
-//     backgroundColor: Colors.orange,
-//     content: ToastErrorWidget(msg),
-//     duration: const Duration(milliseconds: 1000),
-//   );
+void displayToastSuccess(BuildContext context, String msg) {
+  SnackBar snackBar = SnackBar(
+    backgroundColor: Colors.orange,
+    content: ToastSuccessWidget(msg),
+    duration: const Duration(milliseconds: 1000),
+  );
 
-//   ScaffoldMessenger.of(context).showSnackBar(snackBar);
-// }
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
