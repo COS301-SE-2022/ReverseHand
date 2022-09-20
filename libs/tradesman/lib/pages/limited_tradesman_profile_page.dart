87e16bda74e273ff00b7d5f3e7001bf7a8cd0b88
arlo216 <charlottevzyl1@gmail.com>
2022-09-20 20:08:44 +0200

    💄 put report buttons in the appbar

--- libs/tradesman/lib/pages/limited_tradesman_profile_page.dart
+++ libs/tradesman/lib/pages/limited_tradesman_profile_page.dart
@@ -1,14 +1,17 @@
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
 import 'package:general/widgets/appbar.dart';
+import 'package:general/widgets/appbar_popup_menu_widget.dart';
 import 'package:general/widgets/long_button_transparent.dart';
 import 'package:redux_comp/actions/admin/app_management/add_user_report_action.dart';
+import 'package:redux_comp/actions/user/reviews/get_user_reviews_action.dart';
 import 'package:redux_comp/models/admin/app_management/models/report_user_details_model.dart';
 import 'package:redux_comp/models/admin/app_management/report_details_model.dart';
 import 'package:redux_comp/models/user_models/user_model.dart';
 import 'package:redux_comp/redux_comp.dart';
 import 'package:consumer/widgets/consumer_navbar.dart';
 import 'package:general/pages/report_page.dart';
+import 'package:tradesman/widgets/reviews/review_widget.dart';
 
 class LimitedTradesmanProfilePage extends StatelessWidget {
   final Store<AppState> store;
@@ -41,7 +44,22 @@ class LimitedTradesmanProfilePage extends StatelessWidget {
                 return Column(children: [
                   //*******************APP BAR WIDGET*********************//
                   AppBarWidget(
-                      title: "PROFILE", store: store, backButton: true),
+                      title: "PROFILE",
+                      store: store,
+                      filterActions:
+                          AppbarPopUpMenuWidget(store: store, functions: {
+                        "Report User": () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ReportPage(
+                                      store: store,
+                                      reportType: "User",
+                                    )),
+                          );
+                        }
+                      }),
+                      backButton: true),
                   //********************************************************//
 
                   //**************HEADING***************/
@@ -69,7 +87,7 @@ class LimitedTradesmanProfilePage extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.fromLTRB(8, 25, 8, 8),
                     child: SizedBox(
-                      height: 70,
+                      height: 150,
                       width: MediaQuery.of(context).size.width / 1.1,
                       child: Container(
                         decoration: BoxDecoration(
@@ -77,22 +95,76 @@ class LimitedTradesmanProfilePage extends StatelessWidget {
                             borderRadius: BorderRadius.circular(10)),
                         child: Padding(
                             padding: const EdgeInsets.all(20.0),
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children:
-                                    //if there is a rating - 1 is the lowest that can be given
-                                    //so not checking if rating is null
-                                    vm.otherUser.statistics.ratingCount != 0
-                                        ? stars
-                                        : [
-                                            //if no rating yet
-                                            const Text(
-                                              "No rating yet",
-                                              style: TextStyle(
-                                                  color: Colors.white70,
-                                                  fontSize: 18),
-                                            )
-                                          ])),
+                            child: Column(
+                              children: [
+                                Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children:
+                                        //if there is a rating - 1 is the lowest that can be given
+                                        //so not checking if rating is null
+                                        vm.otherUser.statistics.ratingCount != 0
+                                            ? stars
+                                            : [
+                                                //if no rating yet
+                                                const Text(
+                                                  "No rating yet",
+                                                  style: TextStyle(
+                                                      color: Colors.white70,
+                                                      fontSize: 18),
+                                                )
+                                              ]),
+                                const Padding(
+                                    padding: EdgeInsets.only(top: 20)),
+                                TransparentLongButtonWidget(
+                                  text: "See Reviews",
+                                  function: () {
+                                    vm.dispatchGetUserReviewsAction();
+
+                                    List<ReviewWidget> reviews = vm
+                                        .userDetails.reviews
+                                        .map((r) => ReviewWidget(review: r))
+                                        .toList();
+
+                                    showModalBottomSheet(
+                                      context: context,
+                                      isScrollControlled: true,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(7.0),
+                                      ),
+                                      builder: (BuildContext context) {
+                                        return SingleChildScrollView(
+                                          child: Column(
+                                            children: [
+                                              //******************CLOSE*****************//
+                                              Padding(
+                                                padding: const EdgeInsets.only(
+                                                    top: 20.0, right: 8),
+                                                child: Align(
+                                                  alignment: Alignment.topRight,
+                                                  child: IconButton(
+                                                      onPressed: () {
+                                                        Navigator.pop(context);
+                                                      },
+                                                      icon: const Icon(
+                                                        Icons.close,
+                                                        color: Colors.black,
+                                                      )),
+                                                ),
+                                              ),
+
+                                              //******************REVIEWS***************//
+                                              ...reviews,
+                                              //****************************************//
+                                            ],
+                                          ),
+                                        );
+                                      },
+                                    );
+                                  },
+                                ),
+                              ],
+                            )),
                       ),
                     ),
                   ),
@@ -154,25 +226,6 @@ class LimitedTradesmanProfilePage extends StatelessWidget {
                       ),
                     ),
                   ),
-
-                  //ADD REVIEWS HERE
-
-                  const Padding(padding: EdgeInsets.only(top: 15)),
-                  TransparentLongButtonWidget(
-                      text: "Report Contractor",
-                      function: () {
-                        //alternate way in view model
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ReportPage(
-                                    store: store,
-                                    reportType: "User",
-                                  )),
-                        );
-                      })
-
-                  //***********************************/
                 ]);
               }),
         ),
@@ -196,13 +249,7 @@ class _Factory extends VmFactory<AppState, LimitedTradesmanProfilePage> {
         addUserReport: (report, user) =>
             dispatch(AddUserReportAction(report: report, user: user)),
         //this doesn't work at the moment
-        pushReportPage: () {
-          dispatch(NavigateAction.pushNamed('/general/report_page',
-              arguments: ReportPage(
-                store: widget!.store,
-                reportType: "User",
-              )));
-        },
+        dispatchGetUserReviewsAction: () => dispatch(GetUserReviewsAction()),
       );
 }
 
@@ -210,8 +257,8 @@ class _Factory extends VmFactory<AppState, LimitedTradesmanProfilePage> {
 class _ViewModel extends Vm {
   final UserModel userDetails;
   final UserModel otherUser;
-  final VoidCallback pushReportPage;
   final void Function(ReportDetailsModel, ReportUserDetailsModel) addUserReport;
+  final VoidCallback dispatchGetUserReviewsAction;
   final bool isWaiting;
 
   _ViewModel(
@@ -219,6 +266,6 @@ class _ViewModel extends Vm {
       required this.otherUser,
       required this.addUserReport,
       required this.isWaiting,
-      required this.pushReportPage})
+      required this.dispatchGetUserReviewsAction})
       : super(equals: [userDetails, isWaiting]);
 }
