9e1e0798dc5d684968eab8fa815d970d20be5655
arlo216 <charlottevzyl1@gmail.com>
2022-09-20 21:29:49 +0200

    🔥 removing more unnecessary code + flutter clean

--- libs/consumer/lib/pages/bid_details_page.dart
+++ libs/consumer/lib/pages/bid_details_page.dart
@@ -45,15 +45,6 @@ class BidDetailsPage extends StatelessWidget {
                       Text('${vm.bid.name}',
                           style: const TextStyle(
                               fontSize: 33, color: Colors.white)),
-                      //do we still want the date?
-                      // Text(
-                      //   timestampToDate(vm.bid.dateCreated),
-                      //   style: const TextStyle(
-                      //     fontSize: 17,
-                      //     color: Colors.white70,
-                      //   ),
-                      // ),
-
                       IconButton(
                         onPressed: vm.dispatchShortListBidAction,
                         icon: Icon(
