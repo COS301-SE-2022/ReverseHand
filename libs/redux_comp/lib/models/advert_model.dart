8556e3ec17300185e62ca3e943eb433fe804f4c5
Michael du Plessis <mdp0101@gmail.com>
2022-09-22 09:19:14 +0200

    :sparkles: integrated advert ranking system

--- libs/redux_comp/lib/models/advert_model.dart
+++ libs/redux_comp/lib/models/advert_model.dart
@@ -63,7 +63,7 @@ class AdvertModel {
   }
 
   factory AdvertModel.fromJson(obj) {
-    return AdvertModel(
+    final AdvertModel advert = AdvertModel(
       id: obj['id'],
       title: obj['title'],
       userId: obj['customer_id'],
@@ -75,6 +75,14 @@ class AdvertModel {
       dateClosed: obj['date_closed']?.toDouble(),
       imageCount: obj['images'],
     );
+
+    double ranking = 0.0;
+    ranking += _rankTitle(advert);
+    ranking += _photoRank(advert);
+    ranking += _rankDescription(advert);
+    ranking += _rankDateCreated(advert);
+
+    return advert.copy(advertRank: ranking);
   }
 
   @override
@@ -106,4 +114,91 @@ class AdvertModel {
         Object.hashAll(images),
         imageCount,
       );
+
+  static double _rankTitle(AdvertModel advert) {
+    double ranking = 0;
+
+    //First check if the title contains the tradetype of a tradesman
+    // if ((advert.title.toLowerCase()).contains(trade.toLowerCase())) {
+    if ((advert.type.toLowerCase()).contains(advert.title.toLowerCase())) {
+      ranking += 2;
+    }
+
+    //Next check if there is more than one word in the title
+    String title = advert.title.trim(); //remove leading and trailing whitespace
+    final splitted = title.split(' '); //get words delimited by space
+
+    if (splitted.length > 1) {
+      ranking += 1;
+    }
+
+    //Next check the length of the title
+    if (title.length > 12 && title.length < 25) {
+      ranking += 1;
+    }
+
+    return ranking;
+  }
+
+  static double _photoRank(AdvertModel advert) {
+    return 0.0; //stub for now
+  }
+
+  static double _rankDescription(AdvertModel advert) {
+    String description = advert.description!.trim();
+    double ranking = 0;
+
+    if (description.length < 100 && description.length > 50) {
+      ranking += 1;
+    }
+
+    //check if description has any possible numbers that might be used
+    //as a hint for dimensions provided.
+    if (description.contains(RegExp(r'[0-9]'))) {
+      ranking += 1;
+    }
+
+    return ranking;
+  }
+
+  static double _rankDateCreated(AdvertModel advert) {
+    double ranking = 0;
+    int sevenDay = 604800; //time in seconds
+    //example format of date: 27-08-2022
+
+    //get the current date as a unix timestamp
+    double currrentDate = (DateTime.now().millisecondsSinceEpoch / 1000);
+
+    if (((advert.dateCreated) / 1000) + sevenDay < currrentDate) {
+      ranking += 1;
+    } else {
+      ranking += 2;
+    }
+
+    return ranking;
+  }
 }
+
+/**
+ * A maximum rating of 10 can be achieved.
+ * 
+ * Ratings are based on Title, Description, Photo and Date created
+ * Title = 4 points max, Description = 2 points max
+ * Photo = 2 points max, Date Created = 2 points max
+ * 
+ * Title: Check the following:
+ * (a). Length of title: 12 < chars < 25 gets 1 else 0 point
+ * (b). More than one word in the Title i.e descriptive Title. 1 point
+ * (c). Title contains tradetype of tradesman 2 points
+ * 
+ * Description: Check the following:
+ * (a). Length of the Description. 1 point if 50<length<100
+ * (b). Presence of any numbers that could potentially be dimensions or quantity
+ *      1 point for this.
+ * 
+ * Photo: 
+ * (a). Presence of Photo equals 2 points straight
+ * 
+ * Date Created: 
+ * (a). Adverts older than 7 days get points else 1 point.
+ */
