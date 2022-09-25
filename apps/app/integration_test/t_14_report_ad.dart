423f35e96673fa2ef867ff2b2f629d66385ead53
Alexander-Muendesi <u20426918@tuks.co.za>
2022-09-25 07:04:42 +0200

    ✅ Integration Test

--- apps/app/integration_test/t_14_report_ad.dart
+++ apps/app/integration_test/t_14_report_ad.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:general/widgets/appbar_popup_menu_widget.dart';
+import 'package:general/widgets/textfield.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:app/main.dart' as app;
 
@@ -7,7 +9,7 @@ void main() {
   //make sure service is initialized first to run on device
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets("Tradesman Place Bid", (WidgetTester tester) async {
+  testWidgets("Tradesman Report Ad", (WidgetTester tester) async {
     app.main(); //start the app from the main function
     await tester.pumpAndSettle();
 
@@ -53,26 +55,25 @@ void main() {
     //////////////////////////////////////////////////////////////
     //Job Info Page
 
-    var placeBid = find.text("Place Bid");
-    expect(placeBid, findsOneWidget);
-
-    await tester.tap(placeBid);
+    await tester.tap(find.byType(AppbarPopUpMenuWidget));
     await tester.pumpAndSettle();
 
-    var proceedBtn = find.text("Proceed");
-    expect(proceedBtn, findsOneWidget);
-
-    await tester.tap(proceedBtn);
+    await tester.tap(find.text("Report Advert"));
     await tester.pumpAndSettle();
 
-    //Step 2 for entering amount
-    var form = find.byType(TextFormField);
-    expect(form, findsOneWidget);
-
-    await tester.enterText(form, "1000");
+    await tester.tap(find.text("Harrassment"));
     await tester.pumpAndSettle();
 
-    await tester.tap(find.text("Submit Bid"));
+    await tester.enterText(find.byType(TextFieldWidget), "Harrasment");
+    await tester.pumpAndSettle();
+
+    await tester.dragUntilVisible(find.text("Submit Job Report"),
+        find.byType(Scaffold), const Offset(0.0, 300));
+    await tester.pumpAndSettle();
+
+    await Future.delayed(const Duration(seconds: 1), () {});
+
+    await tester.tap(find.text("Submit Job Report"), warnIfMissed: false);
     await tester.pumpAndSettle();
   });
 }
