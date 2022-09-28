72e4fb58985e2b0d0ece5d47c3e0797d1b67da45
Michael du Plessis <mdp0101@gmail.com>
2022-09-28 13:00:15 +0200

    :sparkles: Added pull to refresh throughout the app

--- libs/tradesman/lib/pages/view_bids_page.dart
+++ libs/tradesman/lib/pages/view_bids_page.dart
@@ -36,7 +36,7 @@ class TradesmanViewBidsPage extends StatelessWidget {
                   padding: const EdgeInsets.all(8),
                   child: Column(children: [
                     if (vm.bids.isNotEmpty)
-                    ...populateBids(vm.userId, vm.bids, store),
+                      ...populateBids(vm.userId, vm.bids, store),
                     //********IF NO BIDS********************/
                     if (vm.bids.isEmpty)
                       (const Padding(
